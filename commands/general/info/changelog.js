0989814a35ca5ed48d690ea374acdcd6523584eb
Sangoon_Is_Noob <cotwo0139@gmail.com>
2019-12-08 05:13:52 +0000

    Edited changelog.js

--- commands/general/info/changelog.js
+++ commands/general/info/changelog.js
@@ -1,4 +1,3 @@
-const gitInfo = require('git-repo-info')()
 const fetch = require('node-fetch')
 const Discord = require('discord.js')
 
@@ -21,9 +20,10 @@ class Command {
     const { message } = compressed
     const picker = this.client.utils.localePicker
     const locale = compressed.GuildData.locale
+    const gitInfo = require('git-repo-info')()
     const result = await fetch(this.client._options.others.changelog_url + `${gitInfo.abbreviatedSha}.json`).then(res => res.text()).then(res => res)
     if (result === '<html><head></head><body>NOT FOUND</body></html>') {
-      message.channel.send(picker.get(locale, 'COMMANDS_CHANGELOG_NO'))
+      message.channel.send(picker.get(locale, 'COMMANDS_CHANGELOG_NO', { COMMITSHA: gitInfo.abbreviatedSha }))
     } else {
       const obj = JSON.parse(result)
       const embed = new Discord.RichEmbed(Object.assign(obj.locales[locale], obj.footer))
