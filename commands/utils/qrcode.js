907e04c19abfe5e56eea6da94c18a267d725e48d
Sangoon_Is_Noob <cotwo0139@gmail.com>
2019-12-17 02:06:43 +0900

    Updated Typo

--- commands/utils/qrcode.js
+++ commands/utils/qrcode.js
@@ -24,17 +24,17 @@ class Command {
     const image = await canvas.loadImage(args[0])
     const Canvas = canvas.createCanvas(image.width, image.height)
     const ctx = Canvas.getContext('2d')
-    ctx.drawImage(image,0, 0)
-    const qr = new QrCode();
-    qr.callback = function(err, value) {
+    ctx.drawImage(image, 0, 0)
+    const qr = new QrCode()
+    qr.callback = function (err, value) {
       if (err) {
-        console.error(err);
+        console.error(err)
       }
-      console.log(value.result);
-      console.log(value);
-    };
+      console.log(value.result)
+      console.log(value)
+    }
     message.channel.send(new Discord.Attachment(Canvas.toBuffer(), 'image.png'))
-    qr.decode({width: Canvas.width, Canvas: image.height}, Canvas.toBuffer());
+    qr.decode({ width: Canvas.width, Canvas: image.height }, Canvas.toBuffer())
   }
 }
 
