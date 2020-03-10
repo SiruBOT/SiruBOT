c7ec22a6474a30921708fb3b5335020e4f082cc1
Sangoon_Is_Noob <cotwo0139@gmail.com>
2020-03-10 06:33:38 +0000

    Ignore Case-Sensitive

--- commands/audio/shuffle.js
+++ commands/audio/shuffle.js
@@ -19,7 +19,7 @@ class Command {
     const { message, args, guildData } = compressed
     const { locale } = guildData
     const picker = this.client.utils.localePicker
-    const all = (compressed.userPermissions.includes('DJ') || compressed.userPermissions.includes('Administarator')) && ['all', '전체', 'a', '전', '올'].includes(args[0])
+    const all = (compressed.userPermissions.includes('DJ') || compressed.userPermissions.includes('Administarator')) && ['all', '전체', 'a', '전', '올'].includes(args[0].toLowerCase())
     const result = await this.client.audio.queue.shuffle(message.guild.id, message.author.id, all)
     if (!result) return message.channel.send(picker.get(locale, 'COMMANDS_SHUFFLE_NO'))
     message.channel.send(picker.get(locale, all ? 'COMMANDS_SHUFFLE_ALL' : 'COMMANDS_SHUFFLE_YOUR', { NUM: result }))
