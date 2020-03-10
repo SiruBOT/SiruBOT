372eb970efe74854d0c914448e7b36e85ff6c477
Sangoon_Is_Noob <cotwo0139@gmail.com>
2020-03-10 13:28:03 +0000

    Removed shit

--- commands/audio/dj/related.js
+++ commands/audio/dj/related.js
@@ -21,9 +21,7 @@ class Command {
     const locale = compressed.guildData.locale
     const picker = this.client.utils.localePicker
 
-    const val = this.client.utils.find.matchObj({ on: false, off: true, 켜기: false, 끄기: true }, args[0], compressed.guildData.audioPlayrelated)
-    // console.log(val)
-    switch (val) {
+    switch (this.client.utils.find.matchObj({ on: false, off: true, 켜기: false, 끄기: true }, args[0], compressed.guildData.audioPlayrelated)) {
       case true:
         message.channel.send(picker.get(locale, 'COMMANDS_AUDIO_RELATED_OFF'))
         this.client.database.updateGuild(message.guild.id, { $set: { audioPlayrelated: false } })
