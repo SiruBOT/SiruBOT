efeb8a86541ce6cd0d6d650a7810559938aaf003
Sangoon_Is_Noob <cotwo0139@gmail.com>
2020-03-12 08:31:51 +0900

    Changed Checker, Removed Some Description

--- commands/audio/dj/stop.js
+++ commands/audio/dj/stop.js
@@ -13,7 +13,7 @@ class Command {
   }
 
   /**
-   * @param {Object} compressed - Compressed Object (In CBOT)
+   * @param {Object} compressed - Compressed Object
    */
   async run (compressed) {
     const locale = compressed.guildData.locale
@@ -21,8 +21,13 @@ class Command {
     const { message } = compressed
     const Audio = this.client.audio
     if (!Audio.players.get(message.guild.id)) return message.channel.send(picker.get(locale, 'AUDIO_NOPLAYER'))
-    Audio.stop(message.guild.id)
-    message.channel.send(picker.get(locale, 'COMMANDS_AUDIO_STOP_OK'))
+    const interval = setInterval(() => {
+      Audio.stop(message.guild.id)
+      if (!Audio.players.get(message.guild.id)) {
+        clearInterval(interval)
+        message.channel.send(picker.get(locale, 'COMMANDS_AUDIO_STOP_OK'))
+      }
+    })
   }
 }
 
