bc49ce8bde707c33e00d6989b37a10834ffcb43f
Sangoon_Is_Noob <cotwo0139@gmail.com>
2020-06-06 05:27:15 +0900

    Added audiomessage toggle command, locales update

---
+++ src/commands/audio/admin/setaudiomessage.js
@@ -0,0 +1,46 @@
+const { BaseCommand } = require('../../../structures')
+
+class Command extends BaseCommand {
+  constructor (client) {
+    super(
+      client,
+      'setaudiomessage',
+      ['노래메세지설정', 'setamsg'],
+      ['Administrator'],
+      'MODERATION',
+      {
+        audioNodes: false,
+        playingStatus: false,
+        voiceStatus: {
+          listenStatus: false,
+          sameChannel: false,
+          voiceIn: false
+        }
+      },
+      [],
+      false
+    )
+  }
+
+  /**
+   * @param {Object} compressed - Compressed Object
+   */
+  async run (compressed) {
+    const picker = this.client.utils.localePicker
+    const { locale, audioMessage } = compressed.guildData
+    const { message, args } = compressed
+    const filter = { 끄기: false, 켜기: true, on: true, off: false, enable: true, disable: false }
+    switch (this.client.utils.find.matchObj(filter, args.shift(), audioMessage)) {
+      case true:
+        this.client.database.updateGuild(message.guild.id, { $set: { audioMessage: false } })
+        await message.channel.send(picker.get(locale, 'COMMANDS_AUDIOMESSAGE_DISABLE'))
+        break
+      case false:
+        this.client.database.updateGuild(message.guild.id, { $set: { audioMessage: true } })
+        await message.channel.send(picker.get(locale, 'COMMANDS_AUDIOMESSAGE_ENABLE'))
+        break
+    }
+  }
+}
+
+module.exports = Command
