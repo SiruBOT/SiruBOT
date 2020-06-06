6bb429fd7b512fcbff837532c28ac57ca5d01e2a
Sangoon_Is_Noob <cotwo0139@gmail.com>
2020-06-07 06:22:27 +0900

    Please work

--- src/structures/audio/AudioTimer.js
+++ src/structures/audio/AudioTimer.js
@@ -25,6 +25,8 @@ class AudioTimer {
         } catch {
           this.client.logger.warn(`[AudioTimer] Failed to send TimerEndedMessage ${guildId} is channel is invalid?`)
         }
+      } else {
+        this.clearTimer(guildId)
       }
     }, this.timeout)
     this.timers.set(guildId, timer)
@@ -38,7 +40,7 @@ class AudioTimer {
   chkTimer (guildId) {
     const guild = this.client.guilds.cache.get(guildId)
     if (guild.me.voice.channel && guild.me.voice.channel.members && guild.me.voice.channel.members.filter(el => !el.user.bot).filter(el => !el.voice.serverDeaf && !el.voice.selfDeaf).size <= 0) {
-      if (this.timers.get(guildId)) this.clearTimer(guildId)
+      if (this.timers.get(guildId)) return
       this.client.logger.debug(`[AudioTimer] Timer Started ${this.timeout}ms ${guildId}`)
       this.createTimer(guildId)
     }
