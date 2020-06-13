fe8499db110fc5656c748299f7aefe98daa8112d
Sangoon_Is_Noob <cotwo0139@gmail.com>
2020-06-13 12:32:48 +0900

    Added AFK Field

--- src/constant/models/Member.js
+++ src/constant/models/Member.js
@@ -3,7 +3,11 @@ const mongo = require('mongoose')
 const Schema = new mongo.Schema({
   _id: String,
   warningCount: { type: Number, default: 0 },
-  warningArray: { type: Array, default: [] }
+  warningArray: { type: Array, default: [] },
+  afk: {
+    reason: { type: String, default: 'NONE' },
+    status: { type: Boolean, default: false }
+  }
 }, { collection: 'guildMember', versionKey: false })
 
 module.exports = mongo.model('guildMember', Schema)
