7f7741c1df2f37fe517f7eef4bb45a56a6d54aa2
Sangoon_Is_Noob <cotwo0139@gmail.com>
2020-06-13 12:32:35 +0900

    Added playlist command (SoonTM)

---
+++ src/commands/audio/playlist.js
@@ -0,0 +1,31 @@
+const { BaseCommand } = require('../../structures')
+const Discord = require('discord.js')
+const { placeHolderConstant } = require('../../constant')
+const { EMOJI_ARROW_BACKWARD, EMOJI_ARROW_TORWARD, EMOJI_X } = placeHolderConstant
+
+class Command extends BaseCommand {
+  constructor (client) {
+    super(client,
+      'playlist',
+      ['재생목록', 'ㅔㅣ묘ㅣㅑㄴㅅ', 'pl'],
+      ['Everyone'],
+      'MUSIC_GENERAL',
+      {
+        audioNodes: true,
+        playingStatus: false,
+        voiceStatus: {
+          listenStatus: false,
+          sameChannel: false,
+          voiceIn: false
+        }
+      },
+      true
+    )
+  }
+
+  async run (compressed) {
+    const { message, args, guildData } = compressed
+  }
+}
+
+module.exports = Command
