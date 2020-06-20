278565863ab746cbaee702d8b0ec79c07a41de2e
Sangoon_Is_Noob <cotwo0139@gmail.com>
2020-06-20 23:41:14 +0900

    added subscribedPlaylists

--- src/constant/models/User.js
+++ src/constant/models/User.js
@@ -4,7 +4,8 @@ const Schema = new mongo.Schema({
   _id: String,
   is_supporter: { type: Boolean, default: false },
   profile: { type: String, default: 'default' },
-  blacklisted: { type: Boolean, default: false }
+  blacklisted: { type: Boolean, default: false },
+  subscribedPlaylists: { type: Array, default: [] }
 }, { collection: 'globalUser', versionKey: false })
 
 module.exports = mongo.model('globalUser', Schema)
