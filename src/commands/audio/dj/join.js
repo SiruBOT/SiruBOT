56fde1d2bdba074085a901abd79826f7eb649993
Sangoon_Is_Noob <cotwo0139@gmail.com>
2020-07-17 03:20:24 +0900

    Fixed on already connected, but no message

--- src/commands/audio/dj/join.js
+++ src/commands/audio/dj/join.js
@@ -37,19 +37,22 @@ class Command extends BaseCommand {
       if ((loadMessage.editable && silent === false)) await loadMessage.edit(sendContent)
       else if (err && (err !== 'A Player is already established in this channel')) await message.channel.send(sendContent)
     }
-    if (message.member.voice.channelID && !message.member.voice.channel.joinable && !message.member.voice.channel.speakable) {
-      await sendFunc(picker.get(locale, 'AUDIO_NOT_JOINABLE_OR_SPEAKABLE'), true)
-      return false
-    }
+    if (message.member.voice.channelID && !message.member.voice.channel.joinable && !message.member.voice.channel.speakable) return sendFunc(picker.get(locale, 'COMMANDS_AUDIO_JOIN_DUPLICATED', { VOICECHANNEL: voiceChannelID }))
     try {
+      if (this.client.audio.players.get(message.guild.id) && this.client.audio.players.get(message.guild.id).voiceConnection.voiceChannelID === message.member.voice.channelID) {
+        return sendFunc(picker.get(locale, 'COMMANDS_AUDIO_JOIN_DUPLICATED', { VOICECHANNEL: voiceChannelID }))
+      }
       await (() => {
-        if (this.client.audio.players.get(message.guild.id) && this.client.audio.players.get(message.guild.id).voiceConnection.voiceChannelID !== message.member.voice.channelID) return message.guild.me.voice.setChannel(message.member.voice.channelID)
-        else return this.client.audio.join(voiceChannelID, message.guild.id)
+        if (this.client.audio.players.get(message.guild.id) && this.client.audio.players.get(message.guild.id).voiceConnection.voiceChannelID !== message.member.voice.channelID) {
+          return message.guild.me.voice.setChannel(message.member.voice.channelID)
+        } else {
+          return this.client.audio.join(voiceChannelID, message.guild.id)
+        }
       })()
-      sendFunc(picker.get(locale, 'COMMANDS_AUDIO_JOIN_OK', { VOICECHANNEL: voiceChannelID }))
+      await sendFunc(picker.get(locale, 'COMMANDS_AUDIO_JOIN_OK', { VOICECHANNEL: voiceChannelID }))
       return true
     } catch (e) {
-      if (e.message === 'A Player is already established in this channel') sendFunc(picker.get(locale, 'COMMANDS_AUDIO_JOIN_DUPLICATED', { VOICECHANNEL: voiceChannelID }))
+      if (e.message === 'A Player is already established in this channel') await sendFunc(picker.get(locale, 'COMMANDS_AUDIO_JOIN_DUPLICATED', { VOICECHANNEL: voiceChannelID }))
       else await sendFunc(picker.get(locale, 'COMMANDS_AUDIO_JOIN_FAIL', { VOICECHANNEL: voiceChannelID }) + `\`\`\`js\n > ${`${e.name}: ${e.message}`.split('\n').join('\n> ')}\`\`\``, e)
       return e.message === 'A Player is already established in this channel' ? true : e.message
     }
