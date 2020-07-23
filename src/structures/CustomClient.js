7b8d3c91fc9ac8c02a13fba8872316b9fcf346c7
Sangoon_Is_Noob <cotwo0139@gmail.com>
2020-07-24 03:02:29 +0900

    Fixed #7 (Fixed typo)

--- src/structures/CustomClient.js
+++ src/structures/CustomClient.js
@@ -191,7 +191,7 @@ class CustomClient extends Discord.Client {
     switch (type) {
       case 'ping':
         if (!this.shard) value = this.ws.ping
-        else value = await this.shard.fetchClientValues('ws.ping').then(res => (res.reduce((prev, val) => prev + val, 0) / this._options.bot.shard.count).toFixed(1)).catch(0)
+        else value = await this.shard.fetchClientValues('ws.ping').then(res => (res.reduce((prev, val) => prev + val, 0) / this._options.shard.count).toFixed(1)).catch(0)
         return value
       case 'channels':
         if (!this.shard) value = this.channels.cache.size
