5c8bd82b70548c6fc84448172024093c02c5ef3b
Sangoon_Is_Noob <cotwo0139@gmail.com>
2020-07-24 03:11:47 +0900

    Fixed #8

--- src/structures/BaseEvent.js
+++ src/structures/BaseEvent.js
@@ -4,13 +4,15 @@ class BaseEvent {
    * @description - Base Event for CustomClient
    * @param {CustomClient} client - Your Custom Client
    * @param {String} name - Event name
+   * @param {Function} listener - Evernt Listener
    */
-  constructor (client, name) {
+  constructor (client, name, listener) {
     if (!(client instanceof CustomClient)) throw new Error('supplied constructor `client` is must be instance of BaseClient')
     this.client = client
     if (typeof name !== 'string') throw new Error('supplied constructor `name` is must be String')
     this.name = name
-    if (typeof listener !== 'function') throw new Error('supplied constructor `name` is must be Function')
+    if (typeof listener !== 'function') throw new Error('supplied constructor `listener` is must be Function')
+    this.listener = listener
   }
 
   /**
@@ -18,7 +20,6 @@ class BaseEvent {
    * @returns {Promise<*>} - Returns Any Values (includes void)
    */
   async run () {
-
   }
 }
 
