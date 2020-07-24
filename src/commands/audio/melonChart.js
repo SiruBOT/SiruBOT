08e4ce64ea3c387058515d80946bc1c0210d74de
Sangoon_Is_Noob <cotwo0139@gmail.com>
2020-07-25 05:55:57 +0900

    Added parseDate() Co-authored-by: Ju Heon Ryu <saidbysolo@gmail.com>

--- src/commands/audio/melonChart.js
+++ src/commands/audio/melonChart.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js')
 const { BaseCommand } = require('../../structures')
-const moment = require('moment')
+const moment = require('moment-timezone')
 const Melon = require('melon-chart-api')
 
 class Command extends BaseCommand {
   constructor (client) {
     super(client,
       'melon',
-      ['멜론', '멜론차트'],
+      ['멜론', '멜론차트', '라솔말'],
       ['Everyone'],
       'MUSIC_GENERAL',
       {
@@ -23,9 +23,50 @@ class Command extends BaseCommand {
     )
   }
 
-  async run ({ message }) {
-    const embed = new Discord.MessageEmbed()
-    Melon().realtime()
+  async run ({ message, args }) {
+  }
+
+  getMelonEmbed (page, entires) {
+  };
+
+  parseDate (string) {
+    const time = moment().tz('Asia/Seoul')
+    try {
+      if (string.length > 10) throw new Error('Invalid time text')
+      let tokenized = string.split('/') // Source: 1972/11/21 Except: ["1972", "11", "21"]
+      tokenized = tokenized.filter(el => !isNaN(+el)).map(el => Math.abs(el))
+      const segments = [] // YYYY, MM, DD
+      if (tokenized.length > 3) throw new Error('Tokenized Items must be fewer than 3')
+      if (tokenized.length === 0) throw new Error('Tokenized Items not found')
+      if (tokenized.length === 3 && tokenized[0]) {
+        if (+tokenized[0] > 0 && +tokenized[0] < 100) segments.push(+tokenized[0] + 2000)
+        else segments.push(tokenized[0])
+        if (+tokenized[1] > 0 && +tokenized[1] <= 12) segments.push(+tokenized[1])
+        else throw new Error('MM Parsing Failed')
+        if (+tokenized[2] > 0 && +tokenized[2] <= 31) segments.push(+tokenized[2])
+        else throw new Error('YY Parsing Failed')
+      } else if (tokenized.length === 2 && tokenized[0]) {
+        if (+tokenized[0] > 12 && +tokenized[0] < 100) { // 2000s (2015, 2017, 2018, 2099)
+          segments.push(+tokenized[0] + 2000)
+          if (+tokenized[1] <= 12) segments.push(tokenized[1])
+          else segments.push(time.format('MM'))
+          segments.push(time.format('DD'))
+        } else {
+          segments.push(time.format('YYYY'))
+          if (+tokenized[0] <= 12) segments.push(tokenized[0])
+          else segments.push(time.format('MM'))
+          if (+tokenized[1] <= 31) segments.push(tokenized[1])
+          else segments.push(time.format('DD'))
+        }
+      } else if (tokenized.length === 1 && tokenized[0] && !(+tokenized[0] > 31)) {
+        segments.push(time.format('YYYY'))
+        segments.push(time.format('MM'))
+        segments.push(tokenized[0])
+      } else throw new Error('Time Parsing Failed')
+      return segments.join('/')
+    } catch {
+      return time.format('YYYY/MM/DD')
+    }
   }
 }
 
