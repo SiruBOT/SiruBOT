0c95c155dd8c13782480b1d9d0a602705006d8f2
Sangoon_Is_Noob <cotwo0139@gmail.com>
2020-08-01 04:01:56 +0900

    Check Params

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
 const { BaseCommand } = require('../../structures')
+const { UsageFailedError } = require('../../errors')
 
 class Command extends BaseCommand {
   constructor (client) {
@@ -24,6 +25,7 @@ class Command extends BaseCommand {
   async run ({ message, args, guildData }) {
     const picker = this.client.utils.localePicker
     const { locale } = guildData
+    if (args.length <= 0) throw new UsageFailedError(this.name)
     const search = args.shift()
     const filter = (a) => { return a.displayName.toLowerCase() === search.toLowerCase() || a.id === search || a.id === (this.client.utils.find.getUserFromMention(message.guild.members.cache, search) ? this.client.utils.find.getUserFromMention(message.guild.members.cache, search).id : null) || a.user.username.toLowerCase() === search.toLowerCase() }
     const options = {
@@ -41,25 +43,22 @@ class Command extends BaseCommand {
     if (member.user.bot === true) {
       return message.channel.send(picker.get(locale, 'COMMANDS_MOD_WARN_NO_BOT'))
     } else if (member) {
-      this.ban({ member, args, picker, locale, message })
+      await this.ban({ member, args, picker, locale, message })
     }
   }
 
-  ban ({ member, args, picker, locale, message }) {
+  async ban ({ member, args, picker, locale, message }) {
     const why = !(args.join(' ').length === 0 ? null : args.join(' ')) ? picker.get(locale, 'NONE') : args.join(' ')
     const embed = new Discord.MessageEmbed()
-    member.ban().then(() => {
-      embed.setTitle(picker.get(locale, 'BAN_EMBED_TITLE'))
-        .addFields({ name: picker.get(locale, 'BAN_EMBED_COP_TITLE'), value: picker.get(locale, 'WARN_EMBED_ADMIN_DESC', { USER: message.author, TAG: message.author.tag, ID: message.author.id }), inline: true })
-        .addFields({ name: picker.get(locale, 'BAN_EMBED_PRISONER_TITLE'), value: picker.get(locale, 'WARN_EMBED_USER_DESC', { USER: member, TAG: member.user.tag, ID: member.id }), inline: true })
-        .addFields({ name: picker.get(locale, 'BAN_EMBED_INFO_TITLE'), value: picker.get(locale, 'BAN_EMBED_INFO_DESC', { REASON: why }), inline: true })
-        .setColor(this.client._options.embed.ban)
-        .setTimestamp(new Date())
-      message.channel.send(embed)
-      this.client.loggerManager.send('ban', message.guild, { embed })
-    }).catch((e) => {
-      message.channel.send('Errored ' + e.message)
-    })
+    await member.ban()
+    embed.setTitle(picker.get(locale, 'BAN_EMBED_TITLE'))
+      .addFields({ name: picker.get(locale, 'BAN_EMBED_COP_TITLE'), value: picker.get(locale, 'WARN_EMBED_ADMIN_DESC', { USER: message.author, TAG: message.author.tag, ID: message.author.id }), inline: true })
+      .addFields({ name: picker.get(locale, 'BAN_EMBED_PRISONER_TITLE'), value: picker.get(locale, 'WARN_EMBED_USER_DESC', { USER: member, TAG: member.user.tag, ID: member.id }), inline: true })
+      .addFields({ name: picker.get(locale, 'BAN_EMBED_INFO_TITLE'), value: picker.get(locale, 'BAN_EMBED_INFO_DESC', { REASON: why }), inline: true })
+      .setColor(this.client._options.embed.ban)
+      .setTimestamp(new Date())
+    message.channel.send(embed)
+    this.client.loggerManager.send('ban', message.guild, { embed })
   }
 }
 
