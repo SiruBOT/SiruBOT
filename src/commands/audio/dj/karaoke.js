8f11427f60a2211fafbbcd38f363a2d4b2c17f98
sannoob <cotwo0139@gmail.com>
2020-08-10 17:21:29 +0900

    fix: karaoke value NaN

--- src/commands/audio/dj/karaoke.js
+++ src/commands/audio/dj/karaoke.js
@@ -26,6 +26,7 @@ class Command extends BaseCommand {
     const filterVal = this.client.audio.filters.getFilterValue(message.guild.id, 'karaoke')
     if (args.length === 0) return message.channel.send(picker.get(locale, 'COMMANDS_AUDIO_KARAOKE_BASE', { VAL: filterVal ? filterVal.level : picker.get(locale, 'UNSET'), DESC: '' }))
     else {
+      if (!Number.isInteger(Number(args[0]))) return message.channel.send(picker.get(locale, 'INTEGER'))
       if (Number(args[0]) < 0) return message.channel.send(picker.get(locale, 'LOWERTHANX', { NUM: '0' }))
       if (Number(args[0]) > 10) return message.channel.send(picker.get(locale, 'HIGHERTHANX', { NUM: '10' }))
       if (Number(args[0]) === 1) {
