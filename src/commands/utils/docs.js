e2781fc13b3ff301a301d34443fe1017316cd63a
sannoob <cotwo0139@gmail.com>
2020-08-10 15:35:06 +0900

    fix: user id to mention

--- src/commands/utils/docs.js
+++ src/commands/utils/docs.js
@@ -27,7 +27,7 @@ class Command extends BaseCommand {
     const { locale } = guildData
     if (args.length === 0) return message.channel.send(message.author + '\n' + picker.get(locale, 'GENERAL_INPUT_QUERY'))
     const result = await fetch(`https://djsdocs.sorta.moe/v2/embed?src=https://raw.githubusercontent.com/discordjs/discord.js/docs/master.json&q=${args.join()}`).then(res => res.json())
-    if (!result) return message.channel.send(message.author + '\n' + picker.get(locale, 'GENERAL_NO_RESULT'))
+    if (!result) return message.channel.send('<@' + message.author + '>\n' + picker.get(locale, 'GENERAL_NO_RESULT'))
     const embed = new Discord.MessageEmbed(result)
     embed.setFooter(message.author.tag, message.author.displayAvatarURL({ format: 'png', size: 512 }))
     return message.channel.send(embed)
