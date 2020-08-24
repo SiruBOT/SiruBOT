8c1ca3fcf232e17d0dac6cab937d4c1c0f722bfd
sannoob <cotwo0139@gmail.com>
2020-08-25 01:05:59 +0900

    feat: #32

--- src/commands/audio/volume.js
+++ src/commands/audio/volume.js
@@ -1,4 +1,5 @@
 const { BaseCommand } = require('../../structures')
+const { parse } = require('@sirubot/yt-related-scraper/src/Parser')
 
 class Command extends BaseCommand {
   constructor (client) {
@@ -24,15 +25,42 @@ class Command extends BaseCommand {
     const { locale } = guildData
     const picker = this.client.utils.localePicker
     if ((userPermissions.includes('Administrator') || userPermissions.includes('DJ')) && args.length > 0) {
-      if (isNaN(args[0])) return message.channel.send(picker.get(locale, 'COMMANDS_AUDIO_VOLUME_STRING'))
-      if (Number(args[0]) < 1) return message.channel.send(picker.get(locale, 'COMMANDS_AUDIO_VOLUME_UNDER_ONE'))
-      if (Number(args[0]) > 150) return message.channel.send(picker.get(locale, 'COMMANDS_AUDIO_VOLUME_HIGH_HDF'))
-      await this.client.audio.setVolume(message.guild.id, Number(args[0]))
-      message.channel.send(picker.get(locale, 'COMMANDS_AUDIO_VOLUME_CHANGED', { VOLUME: Number(args[0]) }))
+      const parsed = this.parseVol(args[0], guildData.volume)
+      message.channel.send(parsed)
+      if (isNaN(parsed)) return message.channel.send(picker.get(locale, 'COMMANDS_AUDIO_VOLUME_FORMAT'))
+      if (parsed < 1) return message.channel.send(picker.get(locale, 'COMMANDS_AUDIO_VOLUME_UNDER_ONE'))
+      if (parsed > 150) return message.channel.send(picker.get(locale, 'COMMANDS_AUDIO_VOLUME_HIGH_HDF'))
+      await this.client.audio.setVolume(message.guild.id, parsed)
+      message.channel.send(picker.get(locale, 'COMMANDS_AUDIO_VOLUME_CHANGED', { VOLUME: parsed }))
     } else {
       const guildData = await this.client.database.getGuild(message.guild.id)
       return message.channel.send(picker.get(locale, 'COMMANDS_AUDIO_VOLUME_CURRENT', { VOLUME: guildData.volume }))
     }
+  }
+
+  isStarter (char) {
+    return char === '+' || char === '-'
+  }
+
+  parseVol (string, currentVolume) {
+    const starter = string.charAt(0)
+    const value = +string.substr(1)
+    if (!this.isValid(value)) return NaN
+    switch (starter) {
+      case '+':
+        currentVolume += value
+        break
+      case '-':
+        currentVolume -= value
+        break
+      default:
+        currentVolume = value
+    }
+    return currentVolume
+  }
+
+  isValid (any) {
+    return isFinite(any) && !isNaN(any) && Number.isInteger(any)
   }
 }
 
