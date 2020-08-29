1ecd5771a7b8ec5fab6fb87049672c6f1dad9e23
sannoob <cotwo0139@gmail.com>
2020-08-30 01:27:30 +0900

    feat: badges config

---
+++ src/constant/badges.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    id: 'bughunter',
+    displayNamePath: 'BADGES_BUGHUNTER',
+    emoji: '<:bughunter:747895813266604042>'
+  },
+  {
+      id: 'developer',
+      displayNamePath: 'BADGES_DEVELOPER',
+      emoji: '<:developer:748078467848863855>'
+  }
+  {
+    id: 'contributer',
+    displayNamePath: 'BADGES_CONTRIBUTER',
+    emoji: '<:github:747895814172573817>'
+  }
+]
