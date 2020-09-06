0a415ad94d3eafcbd0ae3f2c522c1cbeae7ffea0
sannoob <cotwo0139@gmail.com>
2020-09-07 04:25:02 +0900

    chore(typo): play, search

--- src/commands/audio/search.js
+++ src/commands/audio/search.js
@@ -48,7 +48,7 @@ class Command extends BaseCommand {
       }
     }
 
-    if (!resumed && args.length === 0 && searchStr.length === 0) return message.channel.send(picker.get(locale, 'GENERAL_INPUT_QUERY'))
+    if (!!resumed && args.length === 0 && searchStr.length === 0) return message.channel.send(picker.get(locale, 'GENERAL_INPUT_QUERY'))
     if (this.client.utils.find.validURL(searchStr)) {
       return this.client.commands.get('play').run(compressed)
     } else searchStr = searchPlatForm + searchStr
@@ -56,29 +56,26 @@ class Command extends BaseCommand {
     const searchResult = await Audio.getTrack(searchStr)
     if (this.client.commands.get('play').chkSearchResult(searchResult, picker, locale, message) !== true) return
     const embed = new Discord.MessageEmbed()
-    let string = ''
-    const maxres = 5
+    const maxres = 10
     const slicedNumberArray = Numbers.slice(0, searchResult.tracks.slice(0, maxres).length)
     slicedNumberArray.push(placeHolderConstant.EMOJI_NO)
     const slicedTracks = searchResult.tracks.slice(0, maxres)
-    for (const index in slicedTracks) {
-      string += `${Numbers[index]}  [${this.client.audio.utils.formatTrack(searchResult.tracks[index].info)}](${searchResult.tracks[index].info.uri})\n`
-    }
-    embed.setDescription(string).setColor(this.client.utils.find.getColor(message.guild.me))
+    const string = slicedTracks.map((value, index) => {
+      return `${Numbers[index] || index} [${this.client.audio.utils.formatTrack(value.info)}](${value.uri})`
+    })
+    embed.setDescription(string.join('\n')).setColor(this.client.utils.find.getColor(message.guild.me))
     const m = await message.channel.send(message.author, embed)
     await this.client.utils.message.massReact(m, slicedNumberArray)
     const filter = (reaction, user) => slicedNumberArray.includes(reaction.emoji.name) && user.id === message.author.id
-    const collected = await m.awaitReactions(filter, { time: 15000, errors: ['time'], max: 1 }).then(coll => coll).catch(() => null)
-    if (m.deletable) m.delete()
-    if (!collected) {
-      await message.channel.send(picker.get(locale, 'GENERAL_TIMED_OUT')).then(m => m.delete({ timeout: 5000 }))
-      return
+    try {
+      const collected = await m.awaitReactions(filter, { time: 15000, errors: ['time'], max: 1 })
+      if (m.deletable) m.delete()
+      if (collected.first().emoji.name === placeHolderConstant.EMOJI_NO) return (await message.channel.send(picker.get(locale, 'GENERAL_USER_STOP'))).delete({ timeout: 1500 })
+      this.client.commands.get('play').addQueue(message, slicedTracks[slicedNumberArray.findIndex((el) => el === collected.first().emoji.name)], picker, locale, true)
+    } catch (e) {
+      if (e instanceof Error) throw e
+      return (await message.channel.send(picker.get(locale, 'GENERAL_TIMED_OUT'))).delete({ timeout: 1500 })
     }
-    if (collected.first().emoji.name === placeHolderConstant.EMOJI_NO) {
-      await message.channel.send(picker.get(locale, 'GENERAL_USER_STOP')).then(m => m.delete({ timeout: 5000 }))
-      return
-    }
-    this.client.commands.get('play').addQueue(message, slicedTracks[slicedNumberArray.findIndex((el) => el === collected.first().emoji.name)], picker, locale, true)
   }
 }
 
