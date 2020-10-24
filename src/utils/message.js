ab0de56a206257c336eeb4124ae76ff69bb60eb8
Sannoob <cotwo0139@gmail.com>
2020-10-25 00:49:56 +0900

    feat: improve utils.message.massReact feat: added utils.message.safeEdit

--- src/utils/message.js
+++ src/utils/message.js
@@ -1,13 +1,23 @@
-const Errors = require('../errors')
+const { Message } = require('discord.js')
 /**
- * @param {Discord.Message} message - to React (Message)
- * @param {Array} array - Array to react
+ * @param {Discord.Message} message Message to React
+ * @param {Array} array Array of emojis
  */
-module.exports.massReact = async (message, array) => {
-  for (const item of array) {
-    if (!message.deleted) {
-      await message.react(item).catch(e => { throw new Errors.PermissionError(e, 'ADD_REACTIONS') })
-    }
+module.exports.massReact = (message, array) => {
+  if (array.length > 20) throw new Error('Emoji types must be < 20')
+  return Promise.all(array.map(el => message.react(el)))
+}
+
+/**
+ * @description A Function edit To message if message is deleted, send message to deleted message's channel
+ * @param {Message} editTo message to edit
+ * @param  {...any} args content to edit
+ */
+module.exports.safeEdit = (editTo, ...args) => {
+  if (!(editTo instanceof Message)) throw new Error('editTo must be instance of DiscordJS#Message')
+  if (editTo.deleted) {
+    return editTo.channel.send(...args)
+  } else {
+    return editTo.edit(...args)
   }
-  return true
 }
