2f82303902222fe755df3dc94f39a182b334e9a0
Sannoob <cotwo0139@gmail.com>
2020-11-11 01:58:30 +0900

    feat: fixed youtube regex

--- src/structures/audio/AudioUtils.js
+++ src/structures/audio/AudioUtils.js
@@ -91,7 +91,7 @@ class AudioUtils {
    */
   getvIdfromUrl (url) {
     if (!url) return undefined
-    const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/
+    const regExp = /^.*((youtu\.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/
     const match = url.match(regExp)
     return (match && match[7].length === 11) ? match[7] : undefined
   }
