c145a128e8a9a724d12f5d6ba8e38a6694e9bdde
Park Seonu <cotwo0139@gmail.com>
2021-10-12 19:47:33 +0900

    chore(remove.js): fix check arguments before check permissons

--- src/commands/audio/remove.js
+++ src/commands/audio/remove.js
@@ -26,12 +26,14 @@ class Command extends BaseCommand {
     const picker = this.client.utils.localePicker
     const { locale } = guildData
     if (args.length <= 0) throw new UsageFailedError(this.name)
+    // 권한체크
+    if (!(userPermissions.includes('Administrator') || userPermissions.includes('DJ')) && guildData.queue[index].request !== message.author.id) return message.channel.send(picker.get(locale, 'COMMANDS_AUDIO_REMOVE_NO_PERM'))
+    // 지울수있는지체크
     if (+args[0] <= 0 || !Number.isInteger(+args[0]) || !isFinite(+args[0]) || isNaN(+args[0])) return message.channel.send(picker.get(locale, 'COMMANDS_AUDIO_REMOVE_NAN'))
     if (guildData.queue.length <= 0) return message.channel.send(picker.get(locale, 'COMMANDS_AUDIO_REMOVE_QUEUE_LESS'))
     if (+args[0] <= 0) return message.channel.send(picker.get(locale, 'COMMANDS_AUDIO_REMOVE_NEGATIVE'))
     if (+args[0] > guildData.queue.length) return message.channel.send(picker.get(locale, 'COMMANDS_AUDIO_REMOVE_MORE_QUEUE', { SIZE: guildData.queue.length }))
     const index = +args[0] - 1
-    if (!(userPermissions.includes('Administrator') || userPermissions.includes('DJ')) && guildData.queue[index].request !== message.author.id) return message.channel.send(picker.get(locale, 'COMMANDS_AUDIO_REMOVE_NO_PERM'))
     await this.client.database.updateGuild(message.guild.id, [ // "Cheat Sheet" https://stackoverflow.com/a/62551740
       {
         $set: {
