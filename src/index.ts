d720b39f9f74c268940b1a1c354fab0e13dd0f75
Sannoob <cotwo0139@gmail.com>
2021-11-01 19:39:16 +0900

    chore(index.ts): lint

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-import { Logger } from 'tslog'
-import { ArgumentParser } from 'argparse'
+import { Logger } from "tslog";
+import { ArgumentParser } from "argparse";
 
-const parser:ArgumentParser = new ArgumentParser({ description: 'SiruBOT BootStrapper CLI' })
+const parser: ArgumentParser = new ArgumentParser({
+  description: "SiruBOT BootStrapper CLI",
+});
 
-const log: Logger = new Logger({ name: 'BootStrap' })
+const log: Logger = new Logger({ name: "BootStrap" });
 
+parser.add_argument("-s", "--shard", {
+  help: "Enable auto sharding",
+  default: false,
+});
 
-parser.add_argument('-s', '--shard', { help: 'Enable auto sharding', default: false })
-
-parser.parse_args()
+parser.parse_args();
