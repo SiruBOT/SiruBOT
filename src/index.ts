0b05e00d4a521001720ac516277b9ffa4eebefc4
Sannoob <cotwo0139@gmail.com>
2021-11-01 19:25:51 +0900

    feat(index.ts): add bootstrap

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-// BOOT THINGS
+import { Logger } from 'tslog'
+import { ArgumentParser } from 'argparse'
+
+const parser:ArgumentParser = new ArgumentParser({ description: 'SiruBOT BootStrapper CLI' })
+
+const log: Logger = new Logger({ name: 'BootStrap' })
+
+
+parser.add_argument('-s', '--shard', { help: 'Enable auto sharding', default: false })
+
+parser.parse_args()
