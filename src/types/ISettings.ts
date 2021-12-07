07e6c2823f935ff069350198b6543d3a937dc579
Sannoob <cotwo0139@gmail.com>
2021-12-07 20:27:33 +0900

    feat(settings): update settings property

--- src/types/ISettings.ts
+++ src/types/ISettings.ts
@@ -1,33 +1,44 @@
 import { NodeOptions } from "shoukaku/types";
-interface ISettings {
-  sentryDsn: string | null;
-  shardArgs: string[];
-  execArgv: string[];
-  bot: {
-    activity: {
-      url: string;
-      type: "STREAMING" | "LISTENING" | "WATCHING" | "PLAYING";
+export interface ISettings {
+  root: {
+    sentryDsn: string | null;
+    bot: {
+      activity: {
+        url?: string;
+        type: "STREAMING" | "LISTENING" | "WATCHING" | "PLAYING";
+      };
+      token: string;
+      playing: string;
+      owners: string[];
     };
-    token: string;
-    games: string[];
-    gamesInterval: number;
-    owners: string[];
-  };
-  webhook: {
-    id: string;
-    token: string;
-  };
-  audio: {
-    searchResults: number;
-    timeout: number;
-    nodes: NodeOptions[];
-    relatedRoutePlanner: {
-      enabled: boolean;
-      ipBlocks: string[];
-      excludeIps: string[];
-      retryCount: number;
+    webhook?: {
+      id: string;
+      token: string;
+    };
+    audio: {
+      searchResults: number;
+      timeout: number;
+      nodes: NodeOptions[];
+      relatedRoutePlanner: {
+        enabled: boolean;
+        ipBlocks: string[];
+        excludeIps: string[];
+        retryCount: number;
+      };
+    };
+    database: {
+      mysql: {
+        host: string;
+        port: number;
+        user: string;
+        password: string;
+        database: string;
+      };
+      mongo: {
+        url: string;
+        user: string;
+        pass: string;
+      };
     };
   };
 }
-
-export default ISettings;
