c26f1dedb38c7faa2bfbfe0980382c77eb46c1a8
Sannoob <cotwo0139@gmail.com>
2021-12-09 22:43:58 +0900

    chore(settings.inc.yaml): remove root

--- src/types/ISettings.ts
+++ src/types/ISettings.ts
@@ -1,44 +1,42 @@
 import { NodeOptions } from "shoukaku/types";
 export interface ISettings {
-  root: {
-    sentryDsn: string | null;
-    bot: {
-      activity: {
-        url?: string;
-        type: "STREAMING" | "LISTENING" | "WATCHING" | "PLAYING";
-      };
-      token: string;
-      playing: string;
-      owners: string[];
+  sentryDsn: string | null;
+  bot: {
+    activity: {
+      url?: string;
+      type: "STREAMING" | "LISTENING" | "WATCHING" | "PLAYING";
     };
-    webhook?: {
-      id: string;
-      token: string;
+    token: string;
+    playing: string;
+    owners: string[];
+  };
+  webhook?: {
+    id: string;
+    token: string;
+  };
+  audio: {
+    searchResults: number;
+    timeout: number;
+    nodes: NodeOptions[];
+    relatedRoutePlanner: {
+      enabled: boolean;
+      ipBlocks: string[];
+      excludeIps: string[];
+      retryCount: number;
     };
-    audio: {
-      searchResults: number;
-      timeout: number;
-      nodes: NodeOptions[];
-      relatedRoutePlanner: {
-        enabled: boolean;
-        ipBlocks: string[];
-        excludeIps: string[];
-        retryCount: number;
-      };
+  };
+  database: {
+    mysql: {
+      host: string;
+      port: number;
+      user: string;
+      password: string;
+      database: string;
     };
-    database: {
-      mysql: {
-        host: string;
-        port: number;
-        user: string;
-        password: string;
-        database: string;
-      };
-      mongo: {
-        url: string;
-        user: string;
-        pass: string;
-      };
+    mongo: {
+      url: string;
+      user: string;
+      pass: string;
     };
   };
 }
