0e10f7fc7149d6c83806dace99372963bf1cf5a0
Sannoob <cotwo0139@gmail.com>
2022-01-08 05:53:26 +0900

    typings: add ICommandRequirements, CommandCategories, CommandPermissions

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,8 @@
-export { IBootStrapperArgs } from "./IBootStrapperArgs";
 export { ISettings } from "./ISettings";
+export { IBootStrapperArgs } from "./IBootStrapperArgs";
 export { IGatewayResponse } from "./IGatewayResponse";
+
+// BaseCommand
+export { ICommandRequirements } from "./ICommandRequirements";
+export { CommandCategories } from "./CommandCategories";
+export { CommandPermissions } from "./CommandPermissions";
