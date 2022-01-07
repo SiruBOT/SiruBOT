1cee41314a7cf9c8fe8007cdb5f2a447aeb4bdde
Sannoob <cotwo0139@gmail.com>
2022-01-08 06:05:18 +0900

    feat(structures/index.ts): add BaseCommand, DatabaseHelper, WebhookNotifier to index.ts

--- src/structures/index.ts
+++ src/structures/index.ts
@@ -1,1 +1,5 @@
 export { Client } from "./Client";
+export { BaseCommand } from "./BaseCommand";
+export { DatabaseHelper } from "./DatabaseHelper";
+
+export { WebhookNotifier } from "./WebhookNotifier";
