9b3aefba4296208f557f64bf69958c3ba63faf05
Sannoob <cotwo0139@gmail.com>
2022-01-08 06:04:10 +0900

    feat(BaseCommand.ts): add BaseCommand class

---
+++ src/structures/BaseCommand.ts
@@ -0,0 +1,31 @@
+import type { SlashCommandBuilder } from "@discordjs/builders";
+import type * as Discord from "discord.js";
+import type {
+  CommandCategories,
+  CommandPermissions,
+  ICommandRequirements,
+} from "../types";
+
+export class BaseCommand {
+  slashCommand: SlashCommandBuilder;
+  category: CommandCategories;
+  permissions: CommandPermissions[];
+  requirements: ICommandRequirements;
+
+  constructor(
+    slashCommand: SlashCommandBuilder,
+    category: CommandCategories,
+    permissions: CommandPermissions[],
+    requirements: ICommandRequirements,
+  ) {
+    this.slashCommand = slashCommand;
+    this.category = category;
+    this.permissions = permissions;
+    this.requirements = requirements;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async run(interaction: Discord.Interaction): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+}
