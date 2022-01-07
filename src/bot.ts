1884b4f624ec90a93335560ce3647c824af79473
Sannoob <cotwo0139@gmail.com>
2022-01-08 05:56:22 +0900

    feat(bot.ts): add Logger feat(bot.ts): add cluster configuration for discord-hybrid-sharding feat(bot.ts): add Sentry setup feat(bot.ts): handle uncaughtException, unhandledRejection

--- src/bot.ts
+++ src/bot.ts
@@ -1,31 +1,98 @@
 import Discord from "discord.js";
 import Cluster from "discord-hybrid-sharding";
+import { Logger } from "tslog";
+import * as Sentry from "@sentry/node";
+import { RewriteFrames } from "@sentry/integrations";
 
 import { Client } from "./structures";
+import * as URLUtils from "./utils/URLUtils";
+
 import type { ISettings, IBootStrapperArgs } from "./types";
 
-const argvSettings: ISettings = JSON.parse(process.argv[2]);
-const bootStrapperArgs: IBootStrapperArgs = JSON.parse(process.argv[3]);
+let argvSettings: ISettings;
+let bootStrapperArgs: IBootStrapperArgs;
+
+try {
+  argvSettings = JSON.parse(process.argv[2]);
+  bootStrapperArgs = JSON.parse(process.argv[3]);
+} catch {
+  console.error(process.argv);
+  throw "Failed to parse process.argv[2] and process.argv[3] (Bot config and Bootstrapper args)";
+}
+
+// Setup logger
+const log: Logger = new Logger({
+  // @ts-expect-error Cluster.data.CLUSTER is set after the cluster is spawned
+  name: bootStrapperArgs.shard ? `Cluster ${Cluster.data.CLUSTER}` : "Client",
+  minLevel: bootStrapperArgs.debug ? "debug" : "info",
+});
+
+// Setup Client
 const intents: Discord.Intents = new Discord.Intents([
   "GUILD_MEMBERS",
   "GUILDS",
 ]);
+log.debug(`Client intents: ${intents.toArray().join(", ")}`);
 
 const clientOptions: Discord.ClientOptions = {
   intents,
 };
 
 if (bootStrapperArgs.shard) {
-  // @ts-expect-error Cluster.data.SHARD_LIST is will after bot is spawned
-  clientOptions.shards = Cluster.data.SHARD_LIST; //  A Array of Shard list, which will get spawned
-  // @ts-expect-error Cluster.data.TOTAL_SHARDS is will after bot is spawned
-  clientOptions.shardCount = Cluster.data.TOTAL_SHARDS; // The Number of Total Shards
+  log.debug(
+    "Sharding enabled. setup clientOptions.shards, clientOptions.shardCount"
+  );
+  // @ts-expect-error Cluster.data.SHARD_LIST is set after the cluster is spawned
+  clientOptions.shards = Cluster.data.SHARD_LIST;
+  // @ts-expect-error Cluster.data.TOTAL_SHARDS is set after the cluster is spawned
+  clientOptions.shardCount = Cluster.data.TOTAL_SHARDS;
+  log.info(
+    // @ts-expect-error clientOptions.shards is set after cluster is spawned
+    `Sharding info shards: [ ${clientOptions.shards.join(
+      ", "
+    )} ], shardCount: ${clientOptions.shardCount}`
+  );
 }
 
-const client = new Client(clientOptions, argvSettings);
+log.debug("Create client instance");
+const client = new Client(clientOptions, log, argvSettings, bootStrapperArgs);
 
 if (bootStrapperArgs.shard) {
-  client.cluster = new Cluster.Client(client);
+  log.debug("Sharding enabled. Set Client.cluster to Cluster.Client");
+  client.cluster = new Cluster.Client(client, true);
 }
 
+if (
+  argvSettings?.sentryDsn &&
+  URLUtils.isURL(argvSettings.sentryDsn as string)
+) {
+  log.info(`Sentry enabled, DSN: ${argvSettings.sentryDsn}`);
+  Sentry.init({
+    dsn: argvSettings.sentryDsn,
+    debug: true,
+    tracesSampleRate: 1.0,
+    integrations: [
+      new RewriteFrames({
+        root: global.__dirname,
+      }),
+    ],
+  });
+  if (bootStrapperArgs.shard && client.cluster) {
+    log.debug(`Sentry sets up. set clusterId tag to ${client.cluster.id}`);
+    Sentry.setTag("clusterId", client.cluster.id);
+  }
+} else {
+  log.warn(`Sentry DSN is not a valid URL: ${argvSettings.sentryDsn}`);
+}
+
+// ignore uncaughtException, unhandledRejection
+const fatal = (err: Error): void => {
+  log.fatal(err);
+  Sentry.captureException(err);
+};
+
+process.on("uncaughtException", fatal);
+process.on("unhandledRejection", fatal);
+
+log.debug("Pre configuration complete. Call client.start() function.");
 client.start();
