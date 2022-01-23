f3ba24ce423227ce7825e27ec2419caf2bba0290
Sannoob <cotwo0139@gmail.com>
2022-01-23 18:24:51 +0900

    feat(ICommandREquirements): add guildPermission

--- src/types/ICommandRequirements.ts
+++ src/types/ICommandRequirements.ts
@@ -1,6 +1,8 @@
+import { type PermissionString } from "discord.js";
 export interface ICommandRequirements {
   audioNode: boolean;
   trackPlaying: boolean;
+  guildPermission: PermissionString;
   voiceStatus: {
     listenStatus: boolean;
     sameChannel: boolean;
