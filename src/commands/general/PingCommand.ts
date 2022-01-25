7d32ae8afebf8343e4cab3340c9993004724caa9
Sannoob <cotwo0139@gmail.com>
2022-01-25 23:38:07 +0900

    feat(PingCommand.ts): add basic Ping command

---
+++ src/commands/general/PingCommand.ts
@@ -0,0 +1,38 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import * as Discord from "discord.js";
+
+import { BaseCommand, Client } from "../../structures";
+import { CommandCategories, CommandPermissions } from "../../types";
+
+export default class PingCommand extends BaseCommand {
+  constructor(client: Client) {
+    const slashCommand = new SlashCommandBuilder()
+      .setName("ping")
+      .setDescription("Replies with ping!");
+    super(
+      slashCommand,
+      client,
+      CommandCategories.GENERAL,
+      [CommandPermissions.EVERYONE],
+      {
+        audioNode: false,
+        trackPlaying: false,
+        guildPermission: "SEND_MESSAGES",
+        voiceStatus: {
+          listenStatus: false,
+          sameChannel: false,
+          voiceConnected: false,
+        },
+      }
+    );
+  }
+
+  public async runCommand(
+    interaction: Discord.CommandInteraction<Discord.CacheType>
+  ): Promise<void> {
+    const calculatedPing = interaction.createdTimestamp - new Date().getTime();
+    await interaction.reply({
+      content: `Pong! interaction handle ping: ${calculatedPing}ms\nWebsocket ping: ${interaction.client.ws.ping}ms`,
+    });
+  }
+}
