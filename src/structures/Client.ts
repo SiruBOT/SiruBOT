470a81d7b6711158bd9714add74915691ec499ed
Sannoob <cotwo0139@gmail.com>
2022-01-25 23:31:21 +0900

    feat(Client.ts): add error handle for setupClient feat(Client.ts): add generateGlobPattern method feat(Client.ts): add warpEventFunc for event error handle feat(Client.ts): add loadEvents, loadCommands method feat(Client.ts#loadCommands): add command version check

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -3,24 +3,35 @@ import path from "path";
 import FastGlob from "fast-glob";
 import * as Sentry from "@sentry/node";
 import { REST as DiscordREST } from "@discordjs/rest";
-import { Routes } from "discord-api-types/v9";
 import { BaseCommand } from "./";
 
+import {
+  type APIApplicationCommand,
+  type APIInteraction,
+  type RESTPostAPIApplicationCommandsJSONBody,
+  Routes,
+} from "discord-api-types/v9";
 import type Cluster from "discord-hybrid-sharding";
 import type { Logger } from "tslog";
 import type { IBootStrapperArgs, ISettings } from "../types";
-import { InteractionRouter } from "./InteractionRouter";
+import type { SlashCommandBuilder } from "@discordjs/builders";
+import { BaseEvent } from "./BaseEvent";
 
 class Client extends Discord.Client {
-  cluster?: Cluster.Client;
-  settings: ISettings;
-  bootStrapperArgs: IBootStrapperArgs;
-  log: Logger;
-  restClient: DiscordREST;
+  public cluster?: Cluster.Client;
+  public settings: ISettings;
+  public bootStrapperArgs: IBootStrapperArgs;
+  public log: Logger;
+  private restClient: DiscordREST;
 
-  commands: Discord.Collection<string, BaseCommand>;
+  public commands: Discord.Collection<string, BaseCommand>;
+  public events: Discord.Collection<string, BaseEvent>;
+  // eventFunctions: Discord.Collection<
+  //   string,
+  //   (...args: Discord.ClientEvents[keyof Discord.ClientEvents]) => Promise<void>
+  // >;
 
-  constructor(
+  public constructor(
     clientOptions: Discord.ClientOptions,
     log: Logger,
     botSettings: ISettings,
@@ -33,49 +44,88 @@ class Client extends Discord.Client {
     this.log = log;
 
     this.commands = new Discord.Collection<string, BaseCommand>();
+    this.events = new Discord.Collection<string, BaseEvent>();
+
     this.restClient = new DiscordREST({ version: "9" });
   }
 
   // Setup bot database, load commands, connect lavalink nodes.. setup audio..p
-  async start(): Promise<void> {
+  public async start(): Promise<void> {
     this.once("ready", this.setupClient); // Login -> Ready -> setupClient -> loadCommands -> ...
     this.log.info("Logging into discord...");
     try {
       await this.login(this.settings.bot.token);
       this.restClient.setToken(this.settings.bot.token);
     } catch (err) {
+      Sentry.captureException(err);
       this.log.error(err);
       throw new Error("Failed to login to Discord.");
     }
   }
 
-  async setupClient() {
+  private async setupClient() {
     this.log.debug("Login successful. Setup client...");
     Sentry.captureException(new Error("Discord client ready"));
-    await this.loadCommands(); // TODO: 전부 핸들하고 하나라도 오류나면 process.exit(1)
-    await this.loadEvents();
+    try {
+      await this.loadCommands(); // TODO: 전부 핸들하고 하나라도 오류나면 process.exit(1)
+      await this.loadEvents();
+    } catch (err) {
+      Sentry.captureException(err);
+      this.log.error(err);
+      throw new Error("Failed to setup client.");
+    }
   }
 
-  async loadEvents() {
-    // Testing purpose
-    this.on("interactionCreate", async (interaction: Discord.Interaction) => {
-      // this.log.debug(`Treating interaction ${interaction.id}`);
-      if (interaction.isCommand()) {
-        const command: BaseCommand | undefined = this.commands.get(
-          interaction.commandName
-        );
-        if (!command) return;
-        InteractionRouter.routeInteraction(interaction, command);
-      }
-    });
-  }
-
-  async loadCommands() {
-    this.log.debug("Load commands...");
-    const commandsPattern: string = path // Create pattern for command files
-      .join(__dirname, "..", "..", "src", "commands", "**/*.js")
+  private generateGlobPattern(dirName: string): string {
+    return path // Create pattern for dirName files
+      .join(__dirname, "..", "..", "src", dirName, "**/*.js")
       .split("\\")
       .join("/");
+  }
+
+  // When unhandled event error, log it and send it to Sentry
+  private warpEventFunc(
+    eventInstance: BaseEvent
+  ): (
+    ...args: Discord.ClientEvents[keyof Discord.ClientEvents]
+  ) => Promise<void> {
+    return async (
+      ...args: Discord.ClientEvents[keyof Discord.ClientEvents]
+    ) => {
+      try {
+        await eventInstance.run(...args);
+      } catch (err) {
+        this.log.error("Unhandled event error from " + eventInstance.name);
+        this.log.error(err);
+        Sentry.captureException(err);
+        Sentry.captureException(
+          new Error("Unhandled event error from " + eventInstance.name)
+        );
+      }
+    };
+  }
+
+  private async loadEvents() {
+    const eventsPattern: string = this.generateGlobPattern("events");
+    this.log.debug("Loading events with pattern: " + eventsPattern);
+    const events: string[] = await FastGlob(eventsPattern);
+    this.log.info(`Found ${events.length} events.`);
+
+    for (const eventPath of events) {
+      const EventClass = await import(eventPath);
+      if (!EventClass.default)
+        throw new Error("Event file is missing default export\n" + eventPath);
+      const eventInstance: BaseEvent = new EventClass.default(this);
+      if (!(eventInstance instanceof BaseEvent))
+        throw new Error("Event file is not extends BaseEvent\n" + eventPath);
+      const eventFunc = this.warpEventFunc(eventInstance);
+      this.on(eventInstance.name, eventFunc);
+      this.events.set(eventInstance.name, eventInstance);
+    }
+  }
+
+  private async loadCommands() {
+    const commandsPattern: string = this.generateGlobPattern("commands");
 
     this.log.debug("Loading commands with pattern: " + commandsPattern);
     const commands: string[] = await FastGlob(commandsPattern);
@@ -89,43 +139,121 @@ class Client extends Discord.Client {
         throw new Error(
           "Command file is missing default export\n" + commandPath
         );
-      const commandInstance: BaseCommand = new CommandClass.default();
+      const commandInstance: BaseCommand = new CommandClass.default(this);
       if (!(commandInstance instanceof BaseCommand))
         throw new Error(
-          "Command file is not extending BaseCommand\n" + commandPath
+          "Command file is not extends BaseCommand\n" + commandPath
         );
       this.commands.set(commandInstance.slashCommand.name, commandInstance);
     }
-    this.log.debug(`Loaded ${this.commands.size} commands`);
 
+    this.log.info("Checking commands to update...");
     this.log.debug(
       "Fetch global commands info from applicationCommands endpoint.."
     );
-    // Get applicationCommands from discord api
-    const resp = await this.restClient.get(
+    // Get applicationCommands from discord api (Old thing)
+    const resp: APIInteraction[] | unknown = await this.restClient.get(
       Routes.applicationCommands(this.application?.id as string)
     );
-    // make Array<slashCommand> from this.commands
-    const slashCommandArray: object[] = [...this.commands.values()].map((e) =>
-      e.slashCommand.toJSON()
+
+    if (!Array.isArray(resp))
+      throw new Error(`Failed to get applicationCommands ${resp}`);
+
+    const onlyChatInput: APIApplicationCommand[] = resp.filter(
+      (e) => e.type === 1 // CHAT_INPUT = 1
     );
-    console.log(resp);
-    // Check diffrence between applicationCommands and slashCommandArray
-    // if (JSON.stringify(resp) !== JSON.stringify(slashCommandArray)) {
-    //   // 체크하는 부분 수정해야함
-    //   this.log.debug("Updating global commands...");
-    //   try {
-    //     await this.restClient.put(
-    //       Routes.applicationCommands(this.application?.id as string),
-    //       { body: slashCommandArray }
-    //     );
-    //   } catch (err) {
-    //     this.log.error(err);
-    //     throw new Error("Failed to update global commands");
-    //   }
-    // } else {
-    //   this.log.debug("Global commands are up to date.");
-    // }
+    /**
+     * CHAT_INPUT	1	Slash commands; a text-based command that shows up when a user types "/"
+     * USER	2	A UI-based command that shows up when you right click or tap on a user
+     * MESSAGE	3	A UI-based command that shows up when you right click or tap on a message
+     */
+
+    // make Array<slashCommand> from this.commands
+    const slashCommandArray: Omit<
+      SlashCommandBuilder,
+      "addSubcommand" | "addSubcommandGroup"
+    >[] = [...this.commands.values()].map((e: BaseCommand) => e.slashCommand);
+
+    const toPost: RESTPostAPIApplicationCommandsJSONBody[] = [];
+    const toPatch = new Map<string, RESTPostAPIApplicationCommandsJSONBody>();
+    const toDelete: string[] = onlyChatInput
+      .filter((e) => !this.commands.has(e.name))
+      .map((e) => e.id);
+
+    for (const command of slashCommandArray) {
+      const jsonCommand: RESTPostAPIApplicationCommandsJSONBody =
+        command.toJSON();
+      const apiCommand: APIApplicationCommand = onlyChatInput.filter(
+        (e) => e.name === command.name
+      )[0];
+      // If command is exists on api, check diffrence between api and local commands
+      if (apiCommand) {
+        if (command.description !== apiCommand.description) {
+          toPatch.set(apiCommand.id, jsonCommand);
+          continue; // Skips to next iteration
+        }
+        // Command.options
+        if (
+          JSON.stringify(jsonCommand.options) !==
+          JSON.stringify(apiCommand.options ? apiCommand.options : []) // apiCommand.options possibly undefined
+        ) {
+          toPatch.set(apiCommand.id, jsonCommand);
+          continue;
+        }
+        // default_permission
+        if (
+          jsonCommand.default_permission && // Optional property, so check if exists
+          jsonCommand.default_permission !== apiCommand.default_permission
+        ) {
+          toPatch.set(apiCommand.id, jsonCommand);
+          continue;
+        }
+      } else {
+        toPost.push(jsonCommand);
+      }
+    } // End of loop
+
+    if (toPost.length + toDelete.length + toPatch.size === 0) {
+      this.log.info("All commands are up to date");
+      return;
+    } else {
+      this.log.info("Updating commands...");
+    }
+
+    // toDelete
+    if (toDelete.length > 0) {
+      this.log.debug(
+        `Delete ${toDelete.length} commands not in local commands`
+      );
+      for (const id of toDelete) {
+        this.log.debug(`Deleting command ${id}`);
+        await this.restClient.delete(
+          Routes.applicationCommand(this.application?.id as string, id)
+        );
+      }
+    }
+    // toPost
+    if (toPost.length > 0) {
+      this.log.debug(`Post ${toPost.length} commands not in local commands`);
+      for (const command of toPost) {
+        this.log.debug(`Posting command ${command.name}`);
+        await this.restClient.post(
+          Routes.applicationCommands(this.application?.id as string),
+          { body: command }
+        );
+      }
+    }
+    // toPatch
+    if (toPatch.size > 0) {
+      this.log.debug(`Patch ${toPatch.size} commands with detected changes`);
+      for (const [commandId, commandJson] of toPatch.entries()) {
+        this.log.debug(`Patch command ${commandJson.name}`);
+        await this.restClient.patch(
+          Routes.applicationCommand(this.application?.id as string, commandId),
+          { body: commandJson }
+        );
+      }
+    }
   }
 }
 
