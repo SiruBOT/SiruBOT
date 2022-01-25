f4eee9458a5fb9761827dfc41635eeaed71cba65
Sannoob <cotwo0139@gmail.com>
2022-01-25 23:36:50 +0900

    feat(BaseCommand.ts): change BaseCommand to abstract class feat(BaseCommand.ts): add client property feat(BaseCommand.ts): change SlashCommandBuilder to Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup"> style(BaseCommand.ts): chnage handle to run

--- src/structures/BaseCommand.ts
+++ src/structures/BaseCommand.ts
@@ -1,75 +1,85 @@
 import type { SlashCommandBuilder } from "@discordjs/builders";
 import type * as Discord from "discord.js";
+import type { Client } from ".";
 import type {
   CommandCategories,
   CommandPermissions,
   ICommandRequirements,
 } from "../types";
 
-export class BaseCommand {
-  slashCommand: SlashCommandBuilder;
-  category: CommandCategories;
-  permissions: CommandPermissions[];
-  requirements: ICommandRequirements;
+export abstract class BaseCommand {
+  public slashCommand: Omit<
+    SlashCommandBuilder,
+    "addSubcommand" | "addSubcommandGroup"
+  >;
+  public category: CommandCategories;
+  public permissions: CommandPermissions[];
+  public requirements: ICommandRequirements;
+  protected client: Client;
 
-  constructor(
-    slashCommand: SlashCommandBuilder,
+  public constructor(
+    slashCommand: Omit<
+      SlashCommandBuilder,
+      "addSubcommand" | "addSubcommandGroup"
+    >,
+    client: Client,
     category: CommandCategories,
     permissions: CommandPermissions[],
     requirements: ICommandRequirements
   ) {
     this.slashCommand = slashCommand;
+    this.client = client;
     this.category = category;
     this.permissions = permissions;
     this.requirements = requirements;
   }
 
-  async handleCommand(
+  async runCommand(
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     interaction: Discord.CommandInteraction
   ): Promise<void> {
-    throw new Error("Method not implemented. BaseCommand#onCommand");
+    throw new Error("Method not implemented. BaseCommand#runCommand");
   }
 
-  async handleAutocomplete(
+  async runAutocomplete(
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     interaction: Discord.AutocompleteInteraction
   ): Promise<void> {
-    throw new Error("Method not implemented. BaseCommand#onAutocomplete");
+    throw new Error("Method not implemented. BaseCommand#runAutocomplete");
   }
 
-  async handleButton(
+  async runButton(
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     interaction: Discord.ButtonInteraction
   ): Promise<void> {
-    throw new Error("Method not implemented. BaseCommand#onButton");
+    throw new Error("Method not implemented. BaseCommand#runButton");
   }
 
-  async handleContextMenu(
+  async runContextMenu(
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     interaction: Discord.ContextMenuInteraction
   ): Promise<void> {
-    throw new Error("Method not implemented. BaseCommand#onContextMenu");
+    throw new Error("Method not implemented. BaseCommand#runContextMenu");
   }
 
-  async handleMessageComponent(
+  async runMessageComponent(
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     interaction: Discord.MessageComponentInteraction
   ): Promise<void> {
-    throw new Error("Method not implemented. BaseCommand#onMessageComponent");
+    throw new Error("Method not implemented. BaseCommand#runMessageComponent");
   }
 
-  async handleSelectMenu(
+  async runSelectMenu(
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     interaction: Discord.SelectMenuInteraction
   ): Promise<void> {
-    throw new Error("Method not implemented. BaseCommand#onSelectMenu");
+    throw new Error("Method not implemented. BaseCommand#runSelectMenu");
   }
 
-  async handleUserContextMenu(
+  async runUserContextMenu(
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     interaction: Discord.UserContextMenuInteraction
   ): Promise<void> {
-    throw new Error("Method not implemented. BaseCommand#onUserContextMenu");
+    throw new Error("Method not implemented. BaseCommand#runUserContextMenu");
   }
 }
