c6e0e64d390347fc9e96d5bf42c644b49fe5e842
Sannoob <cotwo0139@gmail.com>
2022-02-07 02:29:40 +0900

    feat(src/models): remove CustomCommand, ErrorInfo schema

--- src/models/CustomCommand.js
+++
@@ -1,9 +0,0 @@
-const mongo = require('mongoose')
-
-const Schema = new mongo.Schema({
-  _id: String,
-  name: String,
-  actions: { type: Array, default: [] }
-}, { collection: 'customCommand', versionKey: false })
-
-module.exports = mongo.model('customCommand', Schema)
