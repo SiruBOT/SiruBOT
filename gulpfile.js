00e63a3fd981924d7af5b4ed8e0612fe9b4f04ca
Sannoob <cotwo0139@gmail.com>
2022-02-07 02:28:30 +0900

    chore(gulpfile.js): add gulp-typescript chore(gulpfile.js): change build pattern to tsconfig.outDir

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const gulp = require("gulp");
-const del = require("del");
+const del = require("del")
+const ts = require("gulp-typescript");
 const log = require("fancy-log");
+const tsProject = ts.createProject("tsconfig.json");
 
 gulp.task("clean", () => {
-  log("Clean build directory...")
-  return del("build/**/*")
+  log("Clean dist folder");
+  return del(tsProject.config.compilerOptions.outDir)
 })
