92354a7737586a21f70bde855650f9234a61d3c2
Sannoob <cotwo0139@gmail.com>
2022-02-21 03:21:16 +0900

    feat(DatabaseHelper.ts): add DatabaseHelper
    Co-authored-by: Ungbeom-Kang <smile2468@users.noreply.github.com>

--- src/structures/DatabaseHelper.ts
+++ src/structures/DatabaseHelper.ts
@@ -1,1 +1,38 @@
-export class DatabaseHelper {}
+import { createConnection, Connection } from "typeorm";
+import {
+  createConnection as createMongoConnection,
+  Connection as MongoConnection,
+} from "mongoose";
+import { Logger } from "tslog";
+
+import { Client } from ".";
+import { User, Guild } from "../database/mysql/entities";
+export class DatabaseHelper {
+  private log: Logger;
+  protected client: Client;
+  public mysqlConn: Connection | undefined;
+  public mongoConn: MongoConnection | undefined;
+  constructor(client: Client) {
+    this.client = client;
+    this.log = client.log.getChildLogger({
+      name: client.log.settings.name,
+      prefix: ["DataBaseHelper"],
+    });
+  }
+
+  public async setup() {
+    this.log.debug("Setup databases...");
+    const mysqlConn: Connection = await createConnection({
+      type: "mysql",
+      entities: [Guild, User],
+      synchronize: true,
+      ...this.client.settings.database.mysql,
+    });
+    this.mysqlConn = mysqlConn;
+
+    // const mongoConn = await createMongoConnection(
+    //   this.client.settings.database.mongodb.url
+    // ).asPromise();
+    // this.mongoConn = mongoConn;
+  }
+}
