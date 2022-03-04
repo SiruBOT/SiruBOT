30a53db470f5b273b79c8b7e0c184a8b25796e7b
Sannoob <cotwo0139@gmail.com>
2022-03-04 23:29:08 +0900

    feat(EmbedFactory): add getTrackEmbed

--- src/utils/EmbedFactory.ts
+++ src/utils/EmbedFactory.ts
@@ -1,10 +1,62 @@
-import { MessageEmbed } from "discord.js";
+import { Client, MessageEmbed, User } from "discord.js";
+import { Formatter } from ".";
 import { version } from "../../package.json";
 import { BOT_NAME, DEFAULT_COLOR } from "../constant/Constants";
+import { ReusableFormatFunction } from "../locales/LocalePicker";
+import { IAudioTrack } from "../types";
+
+//
 export class EmbedFactory {
   static createEmbed(): MessageEmbed {
     return new MessageEmbed()
-      .setFooter({ text: `${BOT_NAME} - ${version}` })
+      .setFooter({ text: EmbedFactory.footerString })
       .setColor(DEFAULT_COLOR);
+  }
+
+  static get footerString(): string {
+    return `${BOT_NAME} - ${version}`;
+  }
+
+  static async getTrackEmbed(
+    client: Client,
+    format: ReusableFormatFunction,
+    track: IAudioTrack
+  ): Promise<MessageEmbed> {
+    const embed: MessageEmbed = EmbedFactory.createEmbed();
+    if (!track.relatedTrack) {
+      const userInfo: User | undefined =
+        client.users.cache.get(track.requesterUserId) ??
+        (await client.users.fetch(track.requesterUserId));
+      if (userInfo) {
+        embed.setAuthor({
+          iconURL:
+            userInfo.avatarURL({ size: 256 }) ?? userInfo.defaultAvatarURL,
+          name: `${userInfo.username}#${userInfo.discriminator}`,
+        });
+      }
+    }
+    if (track.shoukakuTrack.info.author) {
+      embed.setFooter({
+        text: format(
+          "SOURCE",
+          track.shoukakuTrack.info.author,
+          EmbedFactory.footerString
+        ),
+      });
+    }
+    embed.setDescription(
+      `[${Formatter.formatTrack(track.shoukakuTrack, format("LIVESTREAM"))}](${
+        track.shoukakuTrack.info.uri
+      })`
+    );
+    if (
+      track.shoukakuTrack.info.sourceName === "youtube" &&
+      track.shoukakuTrack.info.identifier
+    ) {
+      embed.setThumbnail(
+        `https://img.youtube.com/vi/${track.shoukakuTrack.info.identifier}/maxresdefault.jpg`
+      );
+    }
+    return embed;
   }
 }
