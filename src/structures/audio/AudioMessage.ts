d886ba9207e12ceff5220611545758ab6bb878fc
Sannoob <cotwo0139@gmail.com>
2022-03-07 17:48:59 +0900

    feat(AudioMessage.ts): add AudioMessage class feat(AudioMessage.ts): add format, getReusableFormatFunction (Closure) feat(AudioMessage.ts): add sendMessage method

---
+++ src/structures/audio/AudioMessage.ts
@@ -0,0 +1,69 @@
+import { AnyChannel, MessageOptions, MessagePayload } from "discord.js";
+import { Logger } from "tslog";
+import { Client, DatabaseHelper } from "..";
+import { Guild } from "../../database/mysql/entities";
+import { ReusableFormatFunction } from "../../locales/LocalePicker";
+import * as Sentry from "@sentry/node";
+import locale from "../../locales";
+
+export class AudioMessage {
+  private client: Client;
+  private guildId: string;
+  private channelId: string;
+  private databaseHelper: DatabaseHelper;
+  private log: Logger;
+  constructor(
+    client: Client,
+    guildId: string,
+    channelId: string,
+    databaseHelper: DatabaseHelper,
+    log: Logger
+  ) {
+    this.client = client;
+    this.guildId = guildId;
+    this.channelId = channelId;
+    this.databaseHelper = databaseHelper;
+    this.log = log.getChildLogger({
+      name: log.settings.name + "AudioMessage",
+    });
+  }
+
+  async format(key: string, ...args: string[]): Promise<string> {
+    this.log.debug(`Format key ${key} with guild config ${this.guildId}`);
+    const guildConfig: Guild = await this.databaseHelper.upsertAndFindGuild(
+      this.guildId
+    );
+    return locale.format(guildConfig.guildLocale, key, ...args);
+  }
+
+  async getReusableFormatFunction(): Promise<ReusableFormatFunction> {
+    this.log.debug(
+      `Get reusable format function (for reduce db query) ${this.guildId}`
+    );
+    const guildConfig: Guild = await this.databaseHelper.upsertAndFindGuild(
+      this.guildId
+    );
+    return locale.getReusableFormatFunction(guildConfig.guildLocale);
+  }
+
+  async sendMessage(
+    options: string | MessagePayload | MessageOptions
+  ): Promise<void> {
+    const channel: AnyChannel | null = await this.client.channels.fetch(
+      this.channelId
+    );
+    if (channel?.isText()) {
+      try {
+        await channel.send(options);
+      } catch (error) {
+        Sentry.captureException(error);
+        this.log.warn(
+          `Failed to send audioMessage to ${this.channelId}`,
+          error
+        );
+      }
+    } else {
+      throw new Error("Channel is not textChannel");
+    }
+  }
+}
