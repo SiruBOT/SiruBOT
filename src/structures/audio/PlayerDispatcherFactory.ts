0eb72eb7a31f49179b1c9bc74483ef10ca6a1b3d
Sannoob <cotwo0139@gmail.com>
2022-03-07 19:24:12 +0900

    feat(PlayerDispatcherFactory.ts): add PlayerDispatcherFactory

---
+++ src/structures/audio/PlayerDispatcherFactory.ts
@@ -0,0 +1,34 @@
+import { AudioHandler } from "./AudioHandler";
+import { Client } from "../Client";
+import { ShoukakuPlayer } from "shoukaku";
+import { Logger } from "tslog";
+import { PlayerDispatcher } from "./PlayerDispatcher";
+
+export class PlayerDispatcherFactory {
+  public audio: AudioHandler;
+  public client: Client;
+  private log: Logger;
+  constructor(audio: AudioHandler) {
+    this.audio = audio;
+    this.log = this.audio.getLogger();
+  }
+
+  async createPlayerDispatcher(
+    player: ShoukakuPlayer,
+    textChannelId: string
+  ): Promise<PlayerDispatcher> {
+    const databaseHelper = this.audio.client.databaseHelper;
+    const playerDispatcher = new PlayerDispatcher(
+      this.audio,
+      player,
+      databaseHelper,
+      textChannelId
+    );
+    playerDispatcher.registerPlayerEvent();
+    // Check resume
+    this.log.debug(
+      `PlayerDispatcher successfully created @ ${player.connection.guildId}/${player.connection.channelId}`
+    );
+    return playerDispatcher;
+  }
+}
