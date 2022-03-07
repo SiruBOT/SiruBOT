49b5ccf571d73a3eca1770268d48dcc74afb1045
Sannoob <cotwo0139@gmail.com>
2022-03-07 14:35:42 +0900

    chore(package.json): bump dependencies (discord-api-types, discord-hybrid-sharding, typescript) chore(package.json): remove gulp-typescript refactor(gulpfile.js): remove gulp-typescript

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
 const gulp = require("gulp");
 const del = require("del")
-const ts = require("gulp-typescript");
 const log = require("fancy-log");
-const tsProject = ts.createProject("tsconfig.json");
+const tsProject = require("./tsconfig.json")
 
 gulp.task("clean", () => {
   log("Clean dist folder");
-  return del(tsProject.config.compilerOptions.outDir)
+  return del(tsProject.compilerOptions.outDir);
 })
