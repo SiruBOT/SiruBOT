14e2e00a295392cfa00bb028e1754dfe0096aab6
Sannoob <cotwo0139@gmail.com>
2022-03-14 19:04:01 +0900

    feat(NodeInfoCommand.ts): rewrite NodeInfoCommand

--- src/commands/audio/NodeInfoCommand.ts
+++ src/commands/audio/NodeInfoCommand.ts
@@ -1,8 +1,31 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import * as Discord from "discord.js";
+import { Constants, ShoukakuSocket } from "shoukaku";
 import { BaseCommand, Client } from "../../structures";
 import { CommandCategories, CommandPermissions } from "../../types";
+import { EmbedFactory, Formatter } from "../../utils";
+import { ExtendedEmbed } from "../../utils/ExtendedEmbed";
 
+// https://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript/23625419
+const units = ["bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+function niceBytes(x: number): string {
+  let l = 0;
+  let n = parseInt(x.toString(), 10) || 0;
+
+  while (n >= 1024 && ++l) {
+    n = n / 1024;
+  }
+  // include a decimal point and a tenths-place digit if presenting
+  // less than ten of KB or greater units
+  return n.toFixed(n < 10 && l > 0 ? 1 : 0) + " " + units[l];
+}
+
+const STATE_STRING = [
+  "CONNECTING",
+  "CONNECTED",
+  "DISCONNECTING",
+  "DISCONNECTED",
+];
 export default class NodeInfoCommand extends BaseCommand {
   constructor(client: Client) {
     const slashCommand = new SlashCommandBuilder()
@@ -28,5 +51,46 @@ export default class NodeInfoCommand extends BaseCommand {
 
   public async runCommand(
     interaction: Discord.CommandInteraction
-  ): Promise<void> {}
+  ): Promise<void> {
+    const nodes: ShoukakuSocket[] = [...this.client.audio.nodes.values()];
+    const onlineNodes: ShoukakuSocket[] = nodes.filter(
+      (v) => v.state === Constants.state.CONNECTED
+    );
+    const totalPlayers: number = onlineNodes
+      .map((e) => e.stats.players)
+      .reduce((a, b) => a + b);
+    const totalPlayingPlayers: number = onlineNodes
+      .map((e) => e.stats.playingPlayers)
+      .reduce((a, b) => a + b);
+    const nodeInfoEmbed: ExtendedEmbed = EmbedFactory.createEmbed();
+    nodeInfoEmbed.setTitle("📡  Node status");
+    nodeInfoEmbed.setTimestamp(new Date());
+    nodeInfoEmbed.setDescription(
+      `All nodes: **${nodes.length}** | Online nodes: **${onlineNodes.length}**\n` +
+        `Total players: **${totalPlayers}** | Total playing players: **${totalPlayingPlayers}**`
+    );
+    for (const node of nodes) {
+      const { cores, lavalinkLoad, systemLoad } = node.stats.cpu;
+      nodeInfoEmbed.addField(
+        `**${node.name}**`,
+        `State: **${STATE_STRING[node.state]}**\n` +
+          `Uptime: **${Formatter.humanizeSeconds(
+            node.stats.uptime / 1000
+          )}**\n` +
+          `Cores: **${cores}**\n` +
+          `Lavalink Load: **${this.formatLoad(lavalinkLoad)}%**\n` +
+          `System Load: **${this.formatLoad(systemLoad)}%**\n` +
+          `Used Memory: **${niceBytes(node.stats.memory.used)}**\n` +
+          `Free Memory: **${niceBytes(node.stats.memory.free)}**\n` +
+          `Frames Sent: **${node.stats.frameStats.sent}**\n` +
+          `Frames Nulled: **${node.stats.frameStats.nulled}**\n` +
+          `Frames Deficit: **${node.stats.frameStats.deficit}**\n`
+      );
+    }
+    interaction.reply({ embeds: [nodeInfoEmbed] });
+  }
+
+  private formatLoad(load: number): string {
+    return Number(load * 100).toFixed(2);
+  }
 }
