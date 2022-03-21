1cba7cd986b24a0ce5702d0beecda7970cd088fe
Sannoob <cotwo0139@gmail.com>
2022-03-21 21:44:42 +0900

    feat(VoiceStateUpdateEvent): add voice connection update

---
+++ src/events/VoiceStateUpdateEvent.ts
@@ -0,0 +1,38 @@
+import { VoiceState } from "discord.js";
+import { BaseEvent, Client } from "../structures";
+import { PlayerDispatcher } from "../structures/audio/PlayerDispatcher";
+import * as Sentry from "@sentry/node";
+
+export default class VoiceStateUpdateEvent extends BaseEvent {
+  constructor(client: Client) {
+    super(client, "voiceStateUpdate");
+  }
+
+  async run(oldState: VoiceState, newState: VoiceState): Promise<void> {
+    if (oldState.member?.id !== this.client.user?.id) return;
+    if (!oldState.channelId) return;
+    if (oldState.channelId !== newState.channelId) {
+      const dispatcher: PlayerDispatcher | undefined =
+        this.client.audio.dispatchers.get(newState.guild.id);
+      this.client.log.debug(
+        `Channel update, ${oldState.channelId} -> ${
+          newState.channelId ? newState.channelId : "Disconnected."
+        }`
+      );
+      if (!dispatcher) return;
+      // Handle Disconnect.
+      if (!newState.channelId) {
+        dispatcher.destroy();
+        await dispatcher.sendDisconnected();
+        return;
+      }
+      try {
+        await dispatcher.player.connection.reconnect(newState.channelId);
+      } catch (error) {
+        Sentry.captureException(error);
+        dispatcher.destroy();
+        this.client.log.error(`Failed to reconnect on moved channel`, error);
+      }
+    }
+  }
+}
