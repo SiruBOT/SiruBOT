709a537c4ead47ea8db4281e715d1c52978b8e1c
Sannoob <cotwo0139@gmail.com>
2022-03-21 21:10:50 +0900

    feat(LyricsCommand): implement LyricsCommand, Autocomplete

--- src/commands/audio/LyricsCommand.ts
+++ src/commands/audio/LyricsCommand.ts
@@ -1,7 +1,15 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import * as Discord from "discord.js";
 import { BaseCommand, Client } from "../../structures";
-import { CommandCategories, CommandPermissions } from "../../types";
+import {
+  CommandCategories,
+  CommandPermissions,
+  HandledCommandInteraction,
+} from "../../types";
+import { ILyricsSearchResult, MelonProvider } from "slyrics";
+import locale from "../../locales";
+import { AutocompleteInteraction, CacheType } from "discord.js";
+import { EmbedFactory } from "../../utils";
+import { AUTOCOMPLETE_MAX_RESULT } from "../../constant/Constants";
 
 export default class LyricsCommand extends BaseCommand {
   constructor(client: Client) {
@@ -12,27 +20,79 @@ export default class LyricsCommand extends BaseCommand {
         return option
           .setName("query")
           .setDescription("가사 검색어")
+          .setAutocomplete(true)
           .setRequired(true);
       });
     super(
       slashCommand,
       client,
-      CommandCategories.GENERAL,
+      CommandCategories.MUSIC,
       [CommandPermissions.EVERYONE],
       {
         audioNode: false,
         trackPlaying: false,
-        guildPermissions: ["SEND_MESSAGES"],
         voiceStatus: {
           listenStatus: false,
           sameChannel: false,
           voiceConnected: false,
         },
-      }
+      },
+      ["SEND_MESSAGES"]
     );
   }
 
   public async runCommand(
-    interaction: Discord.CommandInteraction
-  ): Promise<void> {}
+    interaction: HandledCommandInteraction<"voiceNotConnected">
+  ): Promise<void> {
+    await interaction.deferReply();
+    const query: string = interaction.options.getString("query", true);
+    const provider: MelonProvider = new MelonProvider();
+    const searchResult: ILyricsSearchResult = await provider.search(query);
+    if (searchResult.entries.length <= 0) {
+      await interaction.editReply(
+        locale.format(interaction.locale, "LYRICS_SEARCH_NOT_FOUND")
+      );
+      return;
+    }
+    const resultEmbed = EmbedFactory.createEmbed();
+    const lyrics = await searchResult.entries[0].getLyrics();
+    resultEmbed.setTitle(`${lyrics.title}`);
+    resultEmbed.setFooter({ text: lyrics.artist });
+    if (lyrics.albumCover && typeof lyrics.albumCover === "string")
+      resultEmbed.setThumbnail(lyrics.albumCover as string);
+    // if (lyrics.lyrics.length > 1024) {
+    //   // Pagenation
+    //   resultEmbed.setDescription("Pagenation required (lyrics > 1024)");
+    // } else {
+    resultEmbed.setDescription(
+      lyrics.lyrics ?? locale.format(interaction.locale, "LYRICS_NAN")
+    );
+    // }
+    await interaction.editReply({ embeds: [resultEmbed] });
+  }
+
+  // Autocomplete Handler
+  public async runAutocomplete(
+    interaction: AutocompleteInteraction<CacheType>
+  ): Promise<void> {
+    const query: string | null = interaction.options.getString("query");
+    if (!query) return;
+    const provider: MelonProvider = new MelonProvider();
+    const searchResult: ILyricsSearchResult = await provider.search(query);
+    if (searchResult.entries.length > 0 && !interaction.responded) {
+      await interaction.respond(
+        searchResult.entries
+          .map((result) => {
+            const name = `${result.artist} - ${result.title}`;
+            return {
+              name: name.length > 100 ? name.slice(0, 90) + "..." : name,
+              value: name.length > 100 ? name.slice(0, 99) : name,
+            };
+          })
+          .slice(0, AUTOCOMPLETE_MAX_RESULT)
+      );
+    } else {
+      await interaction.respond([]);
+    }
+  }
 }
