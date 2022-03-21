b5598d20de5722c10b144ca0a372dea4e0a81679
Sannoob <cotwo0139@gmail.com>
2022-03-21 21:43:33 +0900

    feat(VolumeCommand): implement volume command

--- src/commands/audio/VolumeCommand.ts
+++ src/commands/audio/VolumeCommand.ts
@@ -1,14 +1,21 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import * as Discord from "discord.js";
+import { Guild } from "../../database/mysql/entities";
 import { BaseCommand, Client } from "../../structures";
-import { CommandCategories, CommandPermissions } from "../../types";
-
+import {
+  CommandCategories,
+  CommandPermissions,
+  HandledCommandInteraction,
+} from "../../types";
+import { Formatter } from "../../utils";
+import locale from "../../locales";
+import { PlayerDispatcher } from "../../structures/audio/PlayerDispatcher";
 export default class VolumeCommand extends BaseCommand {
   constructor(client: Client) {
     const slashCommand = new SlashCommandBuilder()
       .setName("volume")
       .setDescription("볼륨을 설정하거나 볼 수 있어요")
-      .addStringOption((option) => {
+      .addIntegerOption((option) => {
         return option
           .setName("volume")
           .setDescription("볼륨 (1~150)")
@@ -17,22 +24,66 @@ export default class VolumeCommand extends BaseCommand {
     super(
       slashCommand,
       client,
-      CommandCategories.GENERAL,
+      CommandCategories.MUSIC,
       [CommandPermissions.EVERYONE],
       {
         audioNode: false,
         trackPlaying: false,
-        guildPermissions: ["SEND_MESSAGES"],
         voiceStatus: {
           listenStatus: false,
           sameChannel: false,
           voiceConnected: false,
         },
-      }
+      },
+      ["SEND_MESSAGES"]
     );
   }
 
   public async runCommand(
-    interaction: Discord.CommandInteraction
-  ): Promise<void> {}
+    interaction: HandledCommandInteraction
+  ): Promise<void> {
+    const volume: number | null = interaction.options.getInteger("volume");
+    if (!volume) {
+      const guildConfig: Guild =
+        await this.client.databaseHelper.upsertAndFindGuild(
+          interaction.guildId
+        );
+      await interaction.reply({
+        content: locale.format(
+          interaction.locale,
+          "CURRENT_VOLUME",
+          Formatter.volumeEmoji(guildConfig.volume),
+          guildConfig.volume.toString()
+        ),
+      });
+      return;
+    } else {
+      // Max Volume = 150
+      if (volume > 150) {
+        return await interaction.reply({
+          content: locale.format(interaction.locale, "VOLUME_CANNOT_OVER_MAX"),
+        });
+      } else if (volume < 0) {
+        return await interaction.reply({
+          content: locale.format(interaction.locale, "VOLUME_CANNOT_UNDER_LOW"),
+        });
+      }
+      const guildConfig: Guild =
+        await this.client.databaseHelper.upsertAndFindGuild(
+          interaction.guildId,
+          { volume }
+        );
+      const dispatcher: PlayerDispatcher | undefined =
+        this.client.audio.dispatchers.get(interaction.guildId);
+      if (dispatcher) dispatcher.setVolumePercent(guildConfig.volume);
+      await interaction.reply({
+        content: locale.format(
+          interaction.locale,
+          "CHANGED_VOLUME",
+          Formatter.volumeEmoji(guildConfig.volume),
+          guildConfig.volume.toString()
+        ),
+      });
+    }
+  }
 }
