f7548da77a168ac615e12293daede061ae1396ea
Sannoob <cotwo0139@gmail.com>
2022-04-02 20:33:40 +0900

    feat(QueueCommand): add message title

--- src/commands/audio/QueueCommand.ts
+++ src/commands/audio/QueueCommand.ts
@@ -6,13 +6,12 @@ import {
   CommandPermissions,
   HandledCommandInteraction,
   IAudioTrack,
-  ICommandRequirements,
   IGuildAudioData,
 } from "../../types";
 import { EmbedFactory, Formatter } from "../../utils";
 import { ArrayUtil } from "../../utils/ArrayUtil";
 import { ExtendedEmbed } from "../../utils/ExtendedEmbed";
-import { PageFn, Paginator } from "../../utils/Paginator";
+import { Paginator } from "../../utils/Paginator";
 import locale from "../../locales";
 
 const SPLIT_SIZE = 10;
@@ -54,63 +53,85 @@ export default class QueueCommand extends BaseCommand {
     const { queue, nowPlaying, position } = audioData;
     // trackplaying 이 true이기 때문에 nowplaying 은 체크할 필요 없음
     if (queue.length > 0) {
-      const pageFn: PageFn = async (page: number, maxPage: number) => {
-        const queue: IAudioTrack[] = await dispatcher.queue.getQueue();
-        const chunked: IAudioTrack[][] = ArrayUtil.chunkArray(
-          queue,
-          SPLIT_SIZE
-        );
-        const embed: ExtendedEmbed = EmbedFactory.createEmbed();
-        const pageContent: string = chunked[page - 1]
-          .map((track, index) => {
-            // index = 1 ~ 10
-            // page = 1 ~ end
-            return `**#${index + 1 + (page - 1) * 10}** \`\`[${
-              track.shoukakuTrack.info.length
-                ? Formatter.humanizeSeconds(
-                    track.shoukakuTrack.info.length / 1000
-                  )
-                : "N/A"
-            }]\`\`  ${track.shoukakuTrack.info.title ?? "N/A"} <@${
-              track.requesterUserId
-            }>`;
-          })
-          .join("\n");
-        embed.setTrackThumbnail(queue[0].shoukakuTrack.info);
-        embed.setDescription(pageContent);
-        embed.setFooter({
-          text: locale.format(
-            interaction.locale,
-            "QUEUE_EMBED_FOOTER",
-            queue.length.toString(),
-            Formatter.humanizeSeconds(
-              queue
-                .filter((track) => {
-                  return (
-                    track.shoukakuTrack.info.length &&
-                    !track.shoukakuTrack.info.isStream
-                  );
-                })
-                .reduce((prev, bTrack) => {
-                  return prev + (bTrack.shoukakuTrack.info.length ?? 0);
-                }, 0) / 1000
-            ),
-            page.toString(),
-            maxPage.toString()
-          ),
-        });
-        return { embeds: [embed] };
-      };
       const queuePaginator = new Paginator({
         totalPages: Math.ceil(queue.length / SPLIT_SIZE),
         baseCustomId: "queue_command",
-        pageFn,
+        pageFn: async (page: number, maxPage: number) => {
+          const audioData: IGuildAudioData =
+            await dispatcher.queue.getGuildAudioData();
+          const chunked: IAudioTrack[][] = ArrayUtil.chunkArray(
+            audioData.queue,
+            SPLIT_SIZE
+          );
+          // Embed
+          const embed: ExtendedEmbed = EmbedFactory.createEmbed();
+          const pageContent: string = chunked[page - 1]
+            .map((track, index) => {
+              // index = 1 ~ 10
+              // page = 1 ~ end
+              return `\`\`#${index + 1 + (page - 1) * 10} [${
+                track.shoukakuTrack.info.length
+                  ? Formatter.humanizeSeconds(
+                      track.shoukakuTrack.info.length / 1000
+                    )
+                  : "N/A"
+              }]\`\` | **${track.shoukakuTrack.info.title ?? "N/A"}** <@${
+                track.requesterUserId
+              }>`;
+            })
+            .join("\n");
+          embed.setTrackThumbnail(queue[0].shoukakuTrack.info);
+          embed.setDescription(pageContent);
+          embed.setFooter({
+            text: locale.format(
+              interaction.locale,
+              "QUEUE_EMBED_FOOTER",
+              queue.length.toString(),
+              Formatter.humanizeSeconds(
+                queue
+                  .filter((track) => {
+                    return (
+                      track.shoukakuTrack.info.length &&
+                      !track.shoukakuTrack.info.isStream
+                    );
+                  })
+                  .reduce((prev, bTrack) => {
+                    return prev + (bTrack.shoukakuTrack.info.length ?? 0);
+                  }, 0) / 1000
+              ),
+              page.toString(),
+              maxPage.toString()
+            ),
+          });
+          if (audioData.nowPlaying) {
+            return {
+              embeds: [embed],
+              content: `${this.client.audio.getPlayingState(
+                interaction.guildId
+              )} ${Formatter.formatTrack(
+                audioData.nowPlaying.shoukakuTrack,
+                locale.format(interaction.locale, "LIVESTREAM"),
+                false
+              )}`,
+            };
+          } else {
+            return {
+              embeds: [embed],
+              content: locale.format(interaction.locale, "NOWPLAYING_NONE"),
+            };
+          }
+        },
       });
       await interaction.deferReply();
       await queuePaginator.start(interaction);
     } else {
       await interaction.deferReply();
       await interaction.editReply({
+        content: locale.format(
+          interaction.locale,
+          "NOWPpLAYING_TITLE",
+          dispatcher.player.connection.channelId ?? "N/A"
+        ),
         embeds: [
           await EmbedFactory.getNowplayingEmbed(
             this.client,
