756cc9cf35b366a6b87707f8238b673270377648
Sannoob <cotwo0139@gmail.com>
2022-04-02 21:03:24 +0900

    feat(Constants): add EMOJI_PLAY_STATE

--- src/constant/Constants.ts
+++ src/constant/Constants.ts
@@ -7,11 +7,18 @@ export const OK_COLOR = "#BAFFBA";
 export const WARN_COLOR = "#FFD0D0";
 
 /** Emojis */
-export const EMOJI_REPEAT: { [index: string]: string } = {
-  OFF: "➡️",
-  ALL: "🔁",
-  SINGLE: "🔂",
+export const EMOJI_REPEAT: { [index: number]: string } = {
+  0: "➡️",
+  1: "🔁",
+  2: "🔂",
 };
+
+export const EMOJI_PLAY_STATE: { [index: number]: string } = {
+  0: "▶️",
+  1: "⏹️",
+  2: "⏸️"
+};
+
 export const EMOJI_PLAYLIST = "🗃️";
 export const EMOJI_X = "❌";
 export const EMOJI_INBOX_TRAY = "📥";
