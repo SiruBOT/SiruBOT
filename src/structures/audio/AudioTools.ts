4ab06faf95a87fd79d1766599222e232bbc9bfce
Sannoob <cotwo0139@gmail.com>
2022-04-02 21:03:54 +0900

    refactor(AudioTools): remove unused getNowPlayingEmbed method

--- src/structures/audio/AudioTools.ts
+++ src/structures/audio/AudioTools.ts
@@ -1,18 +1,6 @@
-import { MessageEmbed, User } from "discord.js";
 import { ShoukakuTrack } from "shoukaku";
 import { IAudioTrack } from "../../types";
 export class AudioTools {
-  public static getNowPlayingEmbed(
-    track: ShoukakuTrack,
-    requestUser: User
-  ): MessageEmbed {
-    const embed = new MessageEmbed()
-      .setTitle(requestUser.id)
-      .setColor("#FFBADA")
-      .setDescription(track.info.title || "Title not found.");
-    return embed;
-  }
-
   public static getAudioTrack(
     shoukakuTrack: ShoukakuTrack,
     requesterUserId: string,
