7ea51d38f0afbe5130e80454174eca0306790399
Sannoob <cotwo0139@gmail.com>
2022-04-18 20:35:36 +0900

    feat(SkipCommand): add skip feature when user listen alone

--- src/commands/audio/SkipCommand.ts
+++ src/commands/audio/SkipCommand.ts
@@ -1,8 +1,16 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import * as Discord from "discord.js";
+import { Collection, GuildMember } from "discord.js";
 import { BaseCommand, Client } from "../../structures";
-import { CommandCategories, CommandPermissions } from "../../types";
-
+import { PlayerDispatcher } from "../../structures/audio/PlayerDispatcher";
+import {
+  CommandCategories,
+  CommandPermissions,
+  IAudioTrack,
+  ICommandContext,
+  IGuildAudioData,
+} from "../../types";
+import locale from "../../locales";
+import { EmbedFactory } from "../../utils/EmbedFactory";
 export default class VolumeCommand extends BaseCommand {
   constructor(client: Client) {
     const slashCommand = new SlashCommandBuilder()
@@ -17,7 +25,7 @@ export default class VolumeCommand extends BaseCommand {
       .addIntegerOption((input) => {
         return input
           .setName("to")
-          .setDescription("대기열의 특정 노래로 건너뛸수 있어요")
+          .setDescription("대기열의 특정 노래로 건너뛰기")
           .setRequired(false);
       });
     super(
@@ -38,15 +46,50 @@ export default class VolumeCommand extends BaseCommand {
     );
   }
 
-  public async runCommand(
-    interaction: Discord.CommandInteraction
-  ): Promise<void> {
-    let requiredAdmin = false;
+  public async runCommand({
+    interaction,
+    userPermissions,
+  }: ICommandContext): Promise<void> {
     const forceSkip: boolean | null =
       interaction.options.getBoolean("forceskip");
     const skipTo: number | null = interaction.options.getInteger("to");
-    if (skipTo || forceSkip) requiredAdmin = true;
-    
-    // if (!skipTo) {}
+    const voiceMembers: Collection<string, GuildMember> | undefined =
+      interaction.member.voice.channel?.members;
+    if (voiceMembers) {
+      // Filter voice members without bot and deafened users
+      const actualListenMembers: Collection<string, GuildMember> =
+        voiceMembers.filter(
+          (member) => member.id !== this.client.user?.id && !member.voice.deaf
+        );
+      const dispatcher: PlayerDispatcher =
+        this.client.audio.getPlayerDispatcher(interaction.guildId);
+      const { queue }: IGuildAudioData =
+        await dispatcher.queue.getGuildAudioData();
+      const nextTrack: IAudioTrack | undefined = queue.at(0);
+      if (nextTrack) {
+        // Listen alone
+        if (actualListenMembers.size === 1) {
+          await dispatcher.skipTrack();
+          await interaction.reply({
+            content: locale.format(interaction.locale, "SKIPPED_TRACK"),
+            embeds: [
+              await EmbedFactory.getTrackEmbed(
+                this.client,
+                locale.getReusableFormatFunction(interaction.locale),
+                nextTrack
+              ),
+            ],
+          });
+        } else {
+          // VoteSkip
+          // TODO: Implement voteskip
+        }
+      } else {
+        await dispatcher.cleanStop();
+        await interaction.reply({
+          content: locale.format(interaction.locale, "SKIP_NO_NEXT_STOPPED"),
+        });
+      }
+    }
   }
 }
