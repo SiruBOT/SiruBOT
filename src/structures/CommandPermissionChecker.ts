92f3e24bf2227bc26914fa22942ea1b80f7a9bb4
Sannoob3144 <cotwo0139@gmail.com>
2023-03-22 20:59:20 +0900

    BREAKING CHANGE: Refactor Command Permission Checker, Conditions, DJ Role change

--- src/structures/CommandPermissionChecker.ts
+++ src/structures/CommandPermissionChecker.ts
@@ -1,58 +1,51 @@
-import { Collection, GuildMember } from "discord.js";
+import { GuildMember } from "discord.js";
+import { Client } from "./Client";
 import { Guild } from "../database/mysql/entities";
-import { CommandPermissions, ISettings } from "../types";
+import { CommandPermissions } from "../types/CommandTypes/CommandPermissions";
+import { CommandPermissionConditions } from "../types/CommandTypes/CommandPermissionConditions";
 
-export interface PermissionFilterOptions {
+export interface PermissionCheckOptions {
+  permissionsCheckTo: CommandPermissions[];
   guildMember: GuildMember;
   guildConfig: Guild;
-  settings: ISettings;
 }
 
-const CommandPermissionsFilters: {
-  [index: string]: (options: PermissionFilterOptions) => boolean;
-} = {
-  // Admin
-  [CommandPermissions.ADMIN]: (options: PermissionFilterOptions): boolean => {
-    return options.guildMember.permissions.has("Administrator");
-  },
-  // BotOwner
-  [CommandPermissions.BOTOWNER]: (options: PermissionFilterOptions) =>
-    options.settings.bot.owners.includes(options.guildMember.id),
-  // DJ
-  [CommandPermissions.DJ]: (options: PermissionFilterOptions): boolean => {
-    // TODO: roles Fetch 해서 가져오는 부분이 필요해보임 -> Await/Async 사용해야함
-    // TODO:    -> CommandPermissionChecker 를 async/await지원하게 만들고, onInteractionRe어쩌고에서
-    // TODO:    -> 이벤트에서 처리할때 await으로 처리해야함
-    /**
-     * @description 길드 설정에 dj 역할 아이디가 없다면 관리자가 있거나, 혼자 듣고 있다면 DJ권한을 줌, 만약 길드 설정에 dj 역할이 있다면 멤버에 dj역할이 있는지 확인후 없다면 관리자 있는지 여부
-     */
-    const voiceMembers: Collection<string, GuildMember> | undefined =
-      options.guildMember.voice.channel?.members;
-    if (!options.guildConfig.djRoleId) {
-      return (
-        options.guildMember.permissions.has("Administrator") ||
-        voiceMembers?.filter((member) => !member.user.bot && !member.voice.deaf)
-          ?.size == 1
-      );
-    } else {
-      return (
-        options.guildMember.roles.cache.has(options.guildConfig.djRoleId) ||
-        options.guildMember.permissions.has("Administrator")
-      );
-    }
-  },
-  // Default Permission
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  [CommandPermissions.EVERYONE]: () => true,
+export type PermissionFilterContext = PermissionCheckOptions & {
+  client: Client;
+  permission: CommandPermissions;
 };
 
+export interface PermissionCheckResult {
+  success: boolean;
+  notFulfilledPermissions: CommandPermissions[];
+}
+
 export class CommandPermissionChecker {
-  public static getPermissions(
-    permissionFilterOptions: PermissionFilterOptions
-  ): CommandPermissions[] {
-    return Object.values(CommandPermissions).filter((permission) =>
-      // eslint-disable-next-line security/detect-object-injection
-      CommandPermissionsFilters[permission](permissionFilterOptions)
-    );
+  private client: Client;
+  constructor(client: Client) {
+    this.client = client;
+  }
+
+  /**
+   *  Check if the user has the required permissions
+   * @param {PermissionCheckOptions} options
+   * @returns {Promise<PermissionCheckResult>}
+   */
+  public async check(
+    options: PermissionCheckOptions
+  ): Promise<PermissionCheckResult> {
+    const notFulfilledPermissions: CommandPermissions[] = [];
+    for (const perm of options.permissionsCheckTo) {
+      const checkRes: boolean = await CommandPermissionConditions({
+        ...options,
+        permission: perm,
+        client: this.client,
+      });
+      if (!checkRes) notFulfilledPermissions.push(perm);
+    }
+    return {
+      success: notFulfilledPermissions.length == 0,
+      notFulfilledPermissions,
+    };
   }
 }
