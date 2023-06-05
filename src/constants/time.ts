2e01cff5c50874dc57b38095934c8d57e0fd52e2
Sannoob3144 <cotwo0139@gmail.com>
2023-06-06 01:36:55 +0900

    feat(constants/time): add RELATED_TRACKS_DURATION_OFFSET

--- src/constants/time.ts
+++ src/constants/time.ts
@@ -4,3 +4,5 @@ export const ONE_MINUTE = ONE_SECOND * 60;
 export const BUTTON_AWAIT_TIMEOUT = ONE_SECOND * 20;
 export const PAGINATION_AWAIT_TIMEOUT = ONE_MINUTE;
 export const MESSAGE_CACHE_SWEEPER_INTERVAL = 600;
+/** Related tracks Ranker */
+export const RELATED_TRACKS_DURATION_OFFSET = ONE_MINUTE * 10;
